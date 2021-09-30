b32be36de374514d7a0d2b753040b83c5eda2e01
Dr. Németh Tamás / Tamas Nemeth PhD <tomuwhu@gmail.com>
2021-09-30 09:12:47 +0200

    .

--- py/squarenlist.py
+++ py/squarenlist.py
@@ -1,5 +1,5 @@
 def f( a ):
     return a ** 2
-print( * map( f, range( 10 ) ) )
+print( * map( f, range( 20 ) ) )
 
-print( * [ ( i, i ** 2 ) for i in range( 1, 10 ) ] )
+print( * [ ( i, i ** 2 ) for i in range( 1, 15 ) ] )
