f8053bf650094e346363c9cad8c9c08f3c3c8a4b
Dr. Németh Tamás <tomuwhu@gmail.com>
2021-10-23 00:01:59 +0200

    .

--- py/eratoszthenesz.py
+++ py/eratoszthenesz.py
@@ -6,4 +6,4 @@ for i in range(2, int(sqrt(n)) + 1):
         for j in range(i * i, n, i):
             lst[j] = 0
 print([i for i in range(2, n) if lst[i]])
-# https://hu.wikipedia.org/wiki/Eratoszthen%C3%A9sz_szit%C3%A1ja
+# https://hu.wikipedia.org/wiki/Eratoszthenész_szitája
