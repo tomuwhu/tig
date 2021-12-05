640019ef2dcf5d65e88bfc1e23fcfb940fe8b6c1
Dr. Németh Tamás <tomuwhu@gmail.com>
2021-12-05 15:44:37 +0100

    .

---
+++ py/sumprimes.py
@@ -0,0 +1,13 @@
+# feladat
+# adott egy lista (tosum) pl:
+tosum = [1,2,3,4,5,6,7,8,9,20,21,23]
+# írjunk programot, mely kiírja a listában szereplő prímszámok összegét
+
+from math import sqrt
+n = max(tosum)+1
+lst = [1]*n
+for i in range(2, int(sqrt(n)) + 1):
+    if lst[i]:
+        for j in range(i * i, n, i):
+            lst[j] = 0
+print( sum([ i for i in range(2, n) if lst[i] and i in tosum ]) )
