82a370d47e647aeb296c3e5e40bc27450cefbc65
Dr. Németh Tamás <tomuwhu@gmail.com>
2021-12-07 16:16:25 +0100

    .

--- py/range_while2.py
+++ py/range_while2.py
@@ -1,11 +1,12 @@
 def f(i, n=0, s=1):
     if n==0: (i, n) = (n, i)
     while i < n:
+        yield i
         i += s
-        yield i - s
 print(
     f(10), list( f(10) ), *f(10)
 )
+print("-----")
 print(
     list( f(3, 10, 2) ), 7 in f(3, 10, 2)
 )
