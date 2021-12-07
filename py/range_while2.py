23c95445f7cb8723fdde7c81aa06c697a88dedbf
Dr. Németh Tamás <tomuwhu@gmail.com>
2021-12-07 16:22:04 +0100

    .

--- py/range_while2.py
+++ py/range_while2.py
@@ -4,9 +4,8 @@ def f(i, n=0, s=1):
         yield i
         i += s
 print(
-    f(10), list( f(10) ), *f(10)
+    f(10), list( f(10) ), *map(lambda x:f'{x} - {x ** 2}',f(10)), sep="\n", end="\n------\n"
 )
-print("-----")
 print(
-    list( f(3, 10, 2) ), 7 in f(3, 10, 2)
+    list( f(3, 10, 2) ), 7 in f(3, 10, 2), sep="\n"
 )
