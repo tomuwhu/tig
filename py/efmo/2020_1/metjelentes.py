97ebd811af19ec927f596cfdf1555930c8b0b833
Dr. Németh Tamás <tomuwhu@gmail.com>
2021-12-09 15:10:44 +0100

    .

--- py/efmo/2020_1/metjelentes.py
+++ py/efmo/2020_1/metjelentes.py
@@ -19,13 +19,17 @@ print( f'A legalacsonyabb hőmérséklet: {lo[0]} {lo[1][0:2]}:{lo[1][2:4]} {lo[
 print( f'A legmagasabb hőmérséklet: {hi[0]} {hi[1][0:2]}:{hi[1][2:4]} {hi[3]} fok.' )
 
 print("4. feladat")
-print( 
-    * map(
-        lambda x: f'{x[0]} {x[1][0:2]}:{x[1][2:4]}',
-        list( filter(lambda x: x[2]=='00000', l) )
-    ),
-    sep="\n" 
-)
+sz = list( filter(lambda x: x[2]=='00000', l) )
+if len(sz):
+    print( 
+        * map(
+            lambda x: f'{x[0]} {x[1][0:2]}:{x[1][2:4]}',
+            list( filter(lambda x: x[2]=='00000', l) )
+        ),
+        sep="\n" 
+    )
+else: print("Nem volt szélcsend a mérések idején.")
+
 
 print("5. feladat")
 for v in m:
