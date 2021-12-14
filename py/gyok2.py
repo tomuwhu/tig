5b45f44f363142758e7ae54d8a9579fee7d816ee
Dr. Németh Tamás <tomuwhu@gmail.com>
2021-12-14 18:04:09 +0100

    .

--- py/gyok2.py
+++ py/gyok2.py
@@ -2,7 +2,6 @@ from fractions import Fraction
 def f(x,p):
     s, y = x, 0
     while abs(x-y) > 10 ** -p:
-        y = x
-        x = Fraction((x + Fraction(s, x)), 2) #Babylonian method
+        y, x = x, Fraction(x + Fraction(s, x), 2) #Babylonian method
         yield x.numerator, x.denominator, x.numerator/x.denominator
-print(*f(2,20),sep="\n")
+print(*f(2,20), sep = "\n")
