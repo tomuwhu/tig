036a1eedf585a3eb1ca321ddf0b585d765ee3371
Dr. Németh Tamás / Tamas Nemeth PhD <tomuwhu@gmail.com>
2022-01-05 13:30:27 +0100

    .

--- py/fileread2.py
+++ py/fileread2.py
@@ -1,3 +1,3 @@
 print( # Két dimenziós tömb beolvasása
-    *[[*map(int, i[::2])] for i in map(list, open("inp2.txt"))], sep="\n"
+    [list(map(int, i[::2])) for i in map(list, open("inp2.txt"))]
 )
