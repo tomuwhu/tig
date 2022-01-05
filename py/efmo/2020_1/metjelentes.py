710fd2995286d25e47c8361ab7880a1fcd447bd4
Dr. Németh Tamás / Tamas Nemeth PhD <tomuwhu@gmail.com>
2022-01-05 21:24:58 +0100

    .

--- py/efmo/2020_1/metjelentes.py
+++ py/efmo/2020_1/metjelentes.py
@@ -1,11 +1,11 @@
 "1. feladat"
 m = {}
-def elem(x):
+def f(x):
     y = x.strip().split(sep = " ")
     if y[0] not in m: m[y[0]] = []
     m[y[0]].append(y[1:])
     return y
-l = list( map( elem, open("tavirathu13.txt") ) )
+l = list(map(f, open("tavirathu13.txt")))
 
 print("2. feladat")
 i = m[input("Adja meg egy település kódját! Település: ")][-1][0]
@@ -19,15 +19,10 @@ print( f'A legmagasabb hőmérséklet: {hi[0]} {hi[1][0:2]}:{hi[1][2:4]} {hi[3]}
 
 print("4. feladat")
 sz = list( filter(lambda x: x[2]=='00000', l) )
-if len(sz):
-    print( 
-        * map(
-            lambda x: f'{x[0]} {x[1][0:2]}:{x[1][2:4]}',
-            list( filter(lambda x: x[2]=='00000', l) )
-        ),
-        sep="\n" 
-    )
-else: print("Nem volt szélcsend a mérések idején.")
+print( *map(
+    lambda x: f'{x[0]} {x[1][0:2]}:{x[1][2:4]}',
+    list( filter(lambda x: x[2]=='00000', l) )
+), sep="\n") if len(sz) else print("Nem volt szélcsend a mérések idején.")
 
 print("5. feladat")
 for v in m:
@@ -36,16 +31,10 @@ for v in m:
     pi = max(hi) - min(hi)
     if (set( map( lambda x: x[0][0:2], m[v])) & {"01", "07", "13", "19"} == {"01", "07", "13", "19"}):
         pk = 'Középhőmérséklet: '+str(int(sum(hl)/len(hl)))
-    else:
-        pk = "NA"
+    else: pk = "NA"
     print(f'{v} {pk}; Hőmérséklet-ingadozás: {pi}')
 
 print("6. feladat")
 for v in m:
-    print(
-        v,
-        * list(map( lambda x: f'{x[0][0:2]}:{x[0][2:4]} {int(x[1][-2:])*"#"}', m[v])),
-        sep = "\n",
-        file = open(v + ".txt","w")
-    )
+    print(v, *list(map( lambda x: f'{x[0][0:2]}:{x[0][2:4]} {int(x[1][-2:])*"#"}', m[v])), sep = "\n", file = open(v + ".txt","w"))
 print("A fájlok elkészültek.")
