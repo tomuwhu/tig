fba0de9b86c2b8f5c9e41fc1d17f52521e9ba2cc
Dr. Németh Tamás / Tamas Nemeth PhD <tomuwhu@gmail.com>
2022-01-11 17:44:47 +0100

    .

--- py/p8.py
+++ py/p8.py
@@ -1,10 +1,13 @@
-from functools import reduce
 t = list(map(lambda x: list(map(int, x.split("-"))),open("input.txt")))
+
 print("2. feladat")
 print(f'Az 1. sorban szereplő számok átlaga: {sum(t[0])/len(t[0]):0.2f}')
+
 print("3. feladat")
 t3 = list(map(lambda x: x**2,sorted(t[1])[-8:]))
 print(f'A 2. sorban szereplő 8 legnagyobb szám négyzetének összege: {sum(t3)}')
+
+from functools import reduce
 print("4. feladat")
 t4 = reduce(lambda x, y: x * y, filter(lambda x: x % 3, t[2]), 1)
 print(f'A 3. sorban szereplő 3-mal nem osztható számok szorzatának nagyságrendje: 10^{len(str(t4))}')
