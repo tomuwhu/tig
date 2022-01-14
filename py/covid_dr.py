78247e69137200693f5bfa56ee2611746cffd9cd
Dr. Németh Tamás / Tamas Nemeth PhD <tomuwhu@gmail.com>
2022-01-14 20:11:59 +0100

    .

--- py/covid_dr.py
+++ py/covid_dr.py
@@ -1,3 +1,8 @@
 print(
-    *list(open("covid-dr.csv"))
+    *list(open("covid-drpm.csv")) # Kovid - egy millió főre eső halálozások száma aktuális adat országonként
+    # Forrás: 
+    # owid-covid-data.csv
+    # https://github.com/owid/covid-19-data/tree/master/public/data
+    # https://es6.eu/py/SQL/covid.query
+    # https://es6.eu/py/SQL/covid.db
 )
