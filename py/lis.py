4a5ce53a8b8ed4b139bd064642d706d54a04d68a
Dr. Németh Tamás / Tamas Nemeth PhD <tomuwhu@gmail.com>
2022-01-31 12:59:47 +0100

    .

--- py/lis.py
+++ py/lis.py
@@ -1,8 +1,12 @@
 # https://www.youtube.com/watch?v=aPQY__2H3tE
 def lis(a):
-    l = [1]*len(a)
-    for i in range(1, len(l)):
+    n = len(a); l = [1]*n
+    for i in range(1, n):
         s = [l[k] for k in range(i) if a[k] < a[i]]
         l[i] = 1 + max(s, default = 0)
-    return max(l, default = 0)
-print(lis([5, 2, 8, 6, 3, 5, 9, 7]))
+    mo = []; pr = 1
+    for i in enumerate(l):
+        if i[1] > pr: pr = i[1]; mo.append(i[0]-1)
+    for i in mo: print(a[i], end=", ")
+    print(a[max(enumerate(l), key=lambda x: x[1])[0]])
+lis([5,2,8,6,3,6,9,5])
