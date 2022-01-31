1d8184e42be3cb5ea20a8e4cea55def545ffc317
Dr. Németh Tamás / Tamas Nemeth PhD <tomuwhu@gmail.com>
2022-01-31 22:28:00 +0100

    .

--- py/lis2.py
+++ py/lis2.py
@@ -1,8 +1,10 @@
-# leghosszabb növekvő részsorozat hossza
+# a leghosszabb növekvő részsorozat
 # https://www.youtube.com/watch?v=aPQY__2H3tE
 def lis(a):
-    l = [1]*len(a)
+    l = [1]*len(a); s = {}; sl = []
     for i in range(1, len(a)): 
         l[i] = 1 + max([l[k] for k in range(i) if a[k] < a[i]], default = 0)
-    print(max(l))
+    for i in enumerate(l): s[i[1]] = i[0]
+    for i in s: sl.append((i, s[i]))
+    for i in sorted(sl, key = lambda x: x[0]): print(a[i[1]], end=" ")
 lis([1,5,2,8,3,6,5,4,7,6,9,8,1,4,2,4,3,4,5,7,6,5,4,3,9,1,2,3])
