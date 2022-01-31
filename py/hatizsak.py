a6b0a6f77fb1605399b9d8de353b87164e42d3c3
Dr. Németh Tamás / Tamas Nemeth PhD <tomuwhu@gmail.com>
2022-01-31 08:49:25 +0100

    .

--- py/hatizsak.py
+++ py/hatizsak.py
@@ -1,17 +1,15 @@
-suly = [5, 8, 9, 11, 7, 3, 2]
-fontossag = [1, 7, 8, 6, 3, 9, 4] 
-wmax=20
+# adott tárgyak egy listája, minden tárgynak van mérete és fontossága, továbbá adott egy hátizsák adott mérettel
+# feladat:
+# mennyi a hátizsákba beférő maximális összes fontosság (fontosságok összege)
+t = [(5, 1), (8, 7),(9, 8),(11, 6),(7, 3),(3, 9),(2, 4)] # tárgyak listája (méret, fontosság)
+wmax = 20 # hátizsák mérete
 def hatizsak(i, w):
     if w==0: return 0
     if i==0:
-        if w > suly[0]:
-            return 0
-        else:
-            return fontossag[0]
-    if suly[i] > w:
-        return hatizsak(i - 1, w)
-    return max(hatizsak(i - 1, w), fontossag[i] + hatizsak(i - 1, w-suly[i]))
-
-n=len(suly)
-print("Hátizsak - rekurzív")
-print(f'Idealis berakásnal az összfontossag: {hatizsak(n-1, wmax)}')
+        if w > t[0][0]: return 0
+        else:           return t[0][1]
+    if t[i][0] > w:     return hatizsak(i - 1, w)
+    return max(hatizsak(i - 1, w), t[i][1] + hatizsak(i - 1, w - t[i][0]))
+n=len(t)
+print("Hátizsák - rekurzív")
+print(f'Idealis berakásnal az összfontosság: {hatizsak(n - 1, wmax)}')
