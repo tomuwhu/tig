befa0a95d69608de80c1abb271a9bee1b3489ed5
Dr. Németh Tamás / Tamas Nemeth PhD <tomuwhu@gmail.com>
2022-01-31 10:50:55 +0100

    .

--- linkgy10.md
+++ linkgy10.md
@@ -19,6 +19,7 @@
 [MarkDown](https://www.markdownguide.org/)
 [W3Schools](https://www.w3schools.com/)
 [Videó tananyagok magyar nyelven](https://www.youtube.com/channel/UCs6bOI-3hy2aY2QaE4ZXm0g/videos)
+[Videó tananyagok angol nyelven](https://www.youtube.com/c/MiftaSintahaCS/videos)
 
 ## Könyvek
 
--- py/hatizsak.py
+++ py/hatizsak.py
@@ -2,14 +2,17 @@
 # feladat:
 # mennyi a hátizsákba beférő maximális összes fontosság (fontosságok összege)
 t = [(5, 1), (8, 7),(9, 8),(11, 6),(7, 3),(3, 9),(2, 4)] # tárgyak listája (méret, fontosság)
+n = len(t)
 wmax = 20 # hátizsák mérete
-def hatizsak(i, w):
+def h(i, w):
     if w==0: return 0
     if i==0:
         if w > t[0][0]: return 0
         else:           return t[0][1]
-    if t[i][0] > w:     return hatizsak(i - 1, w)
-    return max(hatizsak(i - 1, w), t[i][1] + hatizsak(i - 1, w - t[i][0]))
-n=len(t)
+    if t[i][0] > w:     return h(i - 1, w)
+    return max(h(i - 1, w), t[i][1] + h(i - 1, w - t[i][0]))
 print("Hátizsák - rekurzív")
-print(f'Idealis berakásnal az összfontosság: {hatizsak(n - 1, wmax)}')
+print(f'Idealis berakásnal az összfontosság: {h(n - 1, wmax)}')
+# https://web.cs.elte.hu/blobs/diplomamunkak/bsc_matelem/2016/horvath_akos.pdf
+# https://prog.hu/tudastar/189582/hatizsak-feladat-megoldasa-dinamikus-programozassal
+# https://www.youtube.com/watch?v=PLJHuErj-Tw
