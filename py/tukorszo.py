1609123f14bace6678aa80c4ec490b7219944aa4
Dr. Németh Tamás / Tamas Nemeth PhD <tomuwhu@gmail.com>
2022-02-01 15:58:10 +0100

    .

--- py/tukorszo.py
+++ py/tukorszo.py
@@ -1,12 +1,7 @@
 def tsz(s):
-    def f(i=0, j=len(s)-1):
+    def f(i = 0, j = len(s) - 1):
         nonlocal s
-        if j <= i:
-            return 0
-        elif s[i] == s[j]:
-            return f(i + 1, j - 1)
-        else:
-            return 1 + min(f(i + 1, j), f(i, j - 1))
+        return 0 if not j > i else f(i + 1, j - 1) if s[i] == s[j] else 1 + min(f(i + 1, j), f(i, j - 1))
     return f()
 print(tsz("géza kék az ég"))
 # http://www.inf.u-szeged.hu/~tnemeth/examples/algoexamples/Tukorszo_forras.html
