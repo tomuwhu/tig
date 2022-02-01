b93cf4a036015f87031dde51866df543ace6990e
Dr. Németh Tamás / Tamas Nemeth PhD <tomuwhu@gmail.com>
2022-02-01 11:34:25 +0100

    .

--- py/lis.py
+++ py/lis.py
@@ -4,5 +4,8 @@ def lis(a):
     l = [1]*len(a)
     for i in range(1, len(a)): 
         l[i] = 1 + max([l[k] for k in range(i) if a[k] < a[i]], default = 0)
-    print(max(l))
-lis([1,5,2,8,3,6,5,4,7,6,9,8,1,4,2,4,3,4,5,7,6,5,4,3,9,1,2,3])
+    return max(l)
+print(
+    lis([1,5,2,8,3,6,5,4,7,6,9,8,1,4,2,4,3,4,5,7,6,5,4,3,9,1,2,3])
+)
+
--- py/lis2.py
+++ py/lis2.py
@@ -1,10 +1,20 @@
 # a leghosszabb növekvő részsorozat
 # https://www.youtube.com/watch?v=aPQY__2H3tE
+from cmath import inf
 def lis(a):
-    l = [1]*len(a); s = {}; sl = []
-    for i in range(1, len(a)): 
-        l[i] = 1 + max([l[k] for k in range(i) if a[k] < a[i]], default = 0)
-    for i in enumerate(l): s[i[1]] = i[0]
-    for i in s: sl.append((i, s[i]))
-    for i in sorted(sl, key = lambda x: x[0]): print(a[i[1]], end=" ")
-lis([1,5,2,8,3,6,5,4,7,6,9,8,1,4,2,4,3,4,5,7,6,5,4,3,9,1,2,3])
+    a = [inf]+a
+    l = [0]*len(a); sl = []
+    for i in range(len(a)): 
+        l[i] = max([(l[k][0] + 1, k, i) for k in range(i) if a[k] < a[i]], default = (1,-1,i), key = lambda x: x[0])
+    m = max(l, key = lambda x: x[0])
+    if m[0]==1: 
+        return [a[1]]
+    while m[1]>0:
+        sl.append(a[m[2]])
+        m = l[m[1]]
+    sl.append(a[m[2]])
+    sl.reverse()
+    return sl
+print(
+    *lis([7,5,3,6,4,2,5])
+)
