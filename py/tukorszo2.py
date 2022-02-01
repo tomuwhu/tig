3ae2984cabce5d595e23262625273061d750fb75
Dr. Németh Tamás / Tamas Nemeth PhD <tomuwhu@gmail.com>
2022-02-01 15:58:50 +0100

    .

--- py/tukorszo2.py
+++ py/tukorszo2.py
@@ -7,8 +7,8 @@ def tsz(s):
             else: t[i][j] = 1 + min(t[i + 1][j], t[i][j - 1])
     i, j = 0, n - 1
     while t[i][j]:
-        if    t[i][j] == t[i + 1][j] + 1:  sl[i] = "*"; i += 1
-        elif  t[i][j] == t[i][j - 1] + 1:  sl[j] = "*"; j -= 1
+        if    t[i][j] == t[i + 1][j] + 1: sl[i] = "*"; i += 1
+        elif  t[i][j] == t[i][j - 1] + 1: sl[j] = "*"; j -= 1
         else: i += 1; j -= 1
     return "".join(sl)
 print(
