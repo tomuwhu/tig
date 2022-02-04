7a3c0ff006c964847192af5b594112d3f6848810
Dr. Németh Tamás / Tamas Nemeth PhD <tomuwhu@gmail.com>
2022-02-04 23:33:19 +0100

    .

--- py/list1.py
+++ py/list1.py
@@ -2,6 +2,7 @@ a = [5,3,5,6,7,8,95,32,43,54,23,12,32,43]
 
 print("Keresés")
 print( 32 in a, 31 in a )
+print(a.index(43))
 
 print("\nLista darabolása")
 print(a[5])     # 5. elem (a sorszámozás 0.-val kezdődik)
@@ -30,19 +31,3 @@ a.sort()
 print( *a )
 
 print("\nRendezés egyedi index alapján\n", *sorted(a, key = lambda x: str(x)[-1]))
-
-# Lista elemű lista
-s = [
-    ["Nagymihály Balázs", "6725", "Szeged", "Pacsirta utca 9.", "+36 (70) 567-45-67"],
-    ["Zsom Olivér", "6720", "Szeged", "Gőzgép utca 29.", "+36 (20) 723-12-73"],
-    ["Balla Petra", "7000", "Kecskemét", "Alkotmány tér 7/a.", "+36 (30) 110-41-12"],
-    ["Szalai Dénes Ferenc", "7000", "Kecskemét", "Alkotmány tér 7/a.", "+36 (30) 110-41-12"],
-    ["Vozár Boglárka", "4578", "Kiskunlacháza", "Kökörcsin út 8.", "+36 (20) 212-11-50"],
-    ["Zsilák Zsuzsanna Edit", "7341", "Kukutyin", "Zabhegyező köz 221.", "+36 (70) 311-40-40"]
-]
-
-print("\nLista elemű lista rendezés adott sorszámú mező szerint:")
-# rendezés adott sorszámú mező szerint
-s.sort(key = lambda x: x[0])
-for i in s:
-    print(f'{i[0]:22s}: {i[4]}')
---
+++ py/list2.py
@@ -0,0 +1,15 @@
+# Lista elemű lista
+s = [
+    ["Nagymihály Balázs", "6725", "Szeged", "Pacsirta utca 9.", "+36 (70) 567-45-67"],
+    ["Zsom Olivér", "6720", "Szeged", "Gőzgép utca 29.", "+36 (20) 723-12-73"],
+    ["Balla Petra", "7000", "Kecskemét", "Alkotmány tér 7/a.", "+36 (30) 110-41-12"],
+    ["Szalai Dénes Ferenc", "7000", "Kecskemét", "Alkotmány tér 7/a.", "+36 (30) 110-41-12"],
+    ["Vozár Boglárka", "4578", "Kiskunlacháza", "Kökörcsin út 8.", "+36 (20) 212-11-50"],
+    ["Zsilák Zsuzsanna Edit", "7341", "Kukutyin", "Zabhegyező köz 221.", "+36 (70) 311-40-40"]
+]
+
+print("\nLista elemű lista rendezés adott sorszámú mező szerint:")
+# rendezés adott sorszámú mező szerint
+s.sort(key = lambda x: x[0])
+for i in s:
+    print(f'{i[0]:22s}: {i[4]}')
