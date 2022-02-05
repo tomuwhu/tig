739d203c3fb6531cf8aa45c6624b877b154f0fc8
Dr. Németh Tamás / Tamas Nemeth PhD <tomuwhu@gmail.com>
2022-02-05 08:42:29 +0100

    .

--- py/colors.py
+++ py/colors.py
@@ -1,3 +1,4 @@
-print("\033[93mCica")
-for i in range(8):
-    print(f'\033[9{i}mCica-{i}')
+from random import randrange as r
+def rgb(s, r, g, b):
+    return f'\N{ESC}[38;2;{r};{g};{b}m{s}\u001b[0m'
+print(rgb("Meglepetés szín",r(50,256), r(50,256), r(50,256)))
