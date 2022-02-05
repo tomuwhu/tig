35eb282b67d4036179a45e6255b39422ad9c7732
Dr. Németh Tamás / Tamas Nemeth PhD <tomuwhu@gmail.com>
2022-02-05 12:58:29 +0100

    .

--- py/strings.py
+++ py/strings.py
@@ -6,7 +6,5 @@ for i in range(32, 127):
     print(f"'{i:c}':{i:d}", end=" ")
 
 print("\n\nTovábbi karakterek:")
-for i in range(8448, 8587):
-    print(f"{i:c}", end=" ")
-for i in range(8592, 9211):
+for i in range(8448, 11390):
     print(f"{i:c}", end=" ")
