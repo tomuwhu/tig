0aa9abcd14172b998527c90932d44a8caaad83ba
Dr. Németh Tamás / Tamas Nemeth PhD <tomuwhu@gmail.com>
2022-02-07 10:49:44 +0100

    .

---
+++ py/inssort.py
@@ -0,0 +1,15 @@
+from random import shuffle as kever
+def insertionSort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >=0 and key < arr[j] :
+                arr[j+1] = arr[j]
+                j -= 1
+        arr[j + 1] = key
+arr = list(range(30))
+kever(arr)
+print(arr)
+insertionSort(arr)
+print ("Sorted array is:")
+print(arr)
