35bc8f35a49a2ec4bf94ef407189040d5bb3cebd
Dr. Németh Tamás / Tamas Nemeth PhD <tomuwhu@gmail.com>
2022-02-09 09:28:41 +0100

    .

---
+++ py/random_ex1.py
@@ -0,0 +1,23 @@
+from random import randrange as r, shuffle as s
+# véletlen egész szám 1..20 között:
+print(r(1,21))
+
+# 10 véletlen egész számból álló lista:
+print([r(1,21) for i in range(10)])
+
+# véletlen egész számbokból álló véletlen elemszámú lista:
+print([r(1,21) for i in range(r(1,21))])
+
+# 10 véletlen egész számból álló monoton növekvő (mn) lista:
+a = 0
+for i in range(10):
+    a += r(0,5)
+    print(a)
+
+# 10 véletlen egész számból álló szigorúan monoton növekvő (szmn) lista:
+a = 0
+l = []
+for i in range(10):
+    a += r(1,10)
+    l.append(a)
+print(l)
