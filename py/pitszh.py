c1fd4c126e1169f24f699a0c43ab7b1aed5cab8c
Dr. Németh Tamás <tomuwhu@gmail.com>
2022-02-23 10:33:26 +0100

    .

--- py/pitszh.py
+++ py/pitszh.py
@@ -1,6 +1,6 @@
 from math import sqrt
 l = set()
-n = 125
+n = 150
 for a in range(2, n):
     for b in range(a + 1, n):
         c = sqrt(a ** 2 + b ** 2)
@@ -11,4 +11,9 @@ for a in range(2, n):
                     mv = False
             if mv:
                 l.add((a, b, int(c)))
-print(sorted(list(l), key=lambda x: x[1] / x[0]))
+def ki(x):
+    a, b, c = x
+    return f"║ {a:5d} ║ {b:5d} ║ {c:5d} ║"
+print("╔═══════╦═══════╦═══════╗")
+print(* map(ki, sorted(list(l), key=lambda x: x[1] / x[0])[:10]), sep="\n╠═══════╬═══════╬═══════╣\n")
+print("╚═══════╩═══════╩═══════╝")
