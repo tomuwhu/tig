1b62efe2d2b14c2da0aee70950f9dd26de99e405
Dr. Németh Tamás <tomuwhu@gmail.com>
2022-02-23 15:29:43 +0100

    .

---
+++ py/pithsz.py
@@ -0,0 +1,21 @@
+from math import sqrt, asin, acos, pi
+l = set()
+n = 150
+for a in range(2, n):
+    for b in range(a + 1, n):
+        c = sqrt(a ** 2 + b ** 2)
+        if c == int(c):
+            mv = True
+            for k in range(1, 30):
+                if (a // k, b // k, int(c) // k) in l:
+                    mv = False
+            if mv:
+                l.add((a, b, int(c)))
+def ki(x):
+    a, b, c = x
+    return f"║ {asin(a/c)*180/pi:5.2f}° ║ {asin(b/c)*180/pi:5.2f}° ║ {a:5d} ║ {b:5d} ║ {c:5d} ║"
+print("╔════════╦════════╦═══════╦═══════╦═══════╗")
+print("║   α    ║   β    ║   a   ║   b   ║   c   ║")
+print("╠════════╬════════╬═══════╬═══════╬═══════╣")
+print(* map(ki, sorted(list(l), key=lambda x: x[1] / x[0])[:40]), sep="\n╠════════╬════════╬═══════╬═══════╬═══════╣\n")
+print("╚════════╩════════╩═══════╩═══════╩═══════╝")
