8fa5ea11398011eae9fbd86768914e98b00af8db
Dr. Németh Tamás <tomuwhu@gmail.com>
2022-02-24 08:03:40 +0100

    .

--- py/table5.py
+++ py/table5.py
@@ -1,13 +1,14 @@
-bf, kf, jf, vv, fv, bk, ba, jk, ja, kk, ka = [" ╔═", "═╦═", "═╗ ", "═══", " ║ ", " ╠═", " ╚═", "═╣ ", "═╝ ", "═╬═", "═╩═" ]
-n = 9
-m = 9
+bf, kf, jf, fv, bk, ba, jk, ja, kk, ka = [" ╔═", "═╦═", "═╗ ", " ║ ", " ╠═", " ╚═", "═╣ ", "═╝ ", "═╬═", "═╩═" ]
+vv = "═" * 3
+n = 10
+m = 10
 for i in range(0,n):
     print(bf if i==0 else bk, end="")
     for j in range(m-1):
         print(vv + (kf if i == 0 else kk), end="")
     print(vv + (jf if i==0 else jk))
     for j in range(m):
-        print(f"{fv}{i+1:1d},{j+1:d}", end="")
+        print(f"{fv}{(i + 1)*(j + 1):3d}", end="")
     print(fv)
 print(ba, end="")
 for j in range(m-1):
