d106257ad3d260291fe492b68ca58314335430f2
Dr. Németh Tamás <tomuwhu@gmail.com>
2022-02-24 16:13:52 +0100

    .

--- py/strings.py
+++ py/strings.py
@@ -5,10 +5,12 @@ print("\nAlap karakterek és kódjaik:")
 for i in range(0x20, 0x300):
     print(f"'{i:c}':{i:x}", end=" ")
 
-print("\n\nTovábbi karakterek:")
-print("  ".join([f"{i:c}" for i in range(0x200, 0x590)]))
-print("  ".join([f"{i:c}" for i in range(0x2100, 0x2427)]))
-print("  ".join([f"{i:c}" for i in range(0x2460, 0x2800)]))
-print("  ".join([f"{i:c}" for i in range(0x2900, 0x2d67)]))
-print("  ".join([f"{i:c}" for i in range(129292, 129535)]))
-print("  ".join([f"{i:c}" for i in range(128512, 128728)]))
+print(
+    "\n\nTovábbi karakterek:\n",
+    "  ".join([f"{i:c}" for i in range(0x200, 0x590)]),
+    "  ".join([f"{i:c}" for i in range(0x2100, 0x2427)]),
+    "  ".join([f"{i:c}" for i in range(0x2460, 0x2800)]),
+    "  ".join([f"{i:c}" for i in range(0x2900, 0x2d67)]),
+    "  ".join([f"{i:c}" for i in range(129292, 129535)]),
+    "  ".join([f"{i:c}" for i in range(128512, 128728)])
+)
