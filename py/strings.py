691d1901acf1aef4d6109f5955e7172d4112fd68
Dr. Németh Tamás <tomuwhu@gmail.com>
2022-02-24 10:01:16 +0100

    .

--- py/strings.py
+++ py/strings.py
@@ -6,9 +6,9 @@ for i in range(32, 127):
     print(f"'{i:c}':{i:d}", end=" ")
 
 print("\n\nTovábbi karakterek:")
-for i in range(128768, 129004):
-    print(f"{i:c}", end=" ")
-for i in range(129292, 129535):
-    print(f"{i:c}", end=" ")
-for i in range(128512, 128728):
-    print(f"{i:c}", end=" ")
+print("  ".join([f"{i:c}" for i in range(0x200, 0x590)]))
+print("  ".join([f"{i:c}" for i in range(0x2100, 0x2427)]))
+print("  ".join([f"{i:c}" for i in range(0x2460, 0x2800)]))
+print("  ".join([f"{i:c}" for i in range(0x2900, 0x2d67)]))
+print("  ".join([f"{i:c}" for i in range(129292, 129535)]))
+print("  ".join([f"{i:c}" for i in range(128512, 128728)]))
