d0bb597fb4ee5a86621a274b9f76346e72617836
Dr. Németh Tamás <tomuwhu@gmail.com>
2022-02-24 10:03:41 +0100

    .

--- py/strings.py
+++ py/strings.py
@@ -2,8 +2,8 @@ s = "cica"
 print(f'ff={2.23455:.2f}, kd="{s}"')
 
 print("\nAlap karakterek és kódjaik:")
-for i in range(32, 127):
-    print(f"'{i:c}':{i:d}", end=" ")
+for i in range(0x20, 0x300):
+    print(f"'{i:c}':{i:x}", end=" ")
 
 print("\n\nTovábbi karakterek:")
 print("  ".join([f"{i:c}" for i in range(0x200, 0x590)]))
