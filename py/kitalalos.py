75962e973fee4118add039c615fb16b592c52ae8
Dr. Németh Tamás / Tamas Nemeth PhD <tomuwhu@gmail.com>
2022-03-18 14:11:11 +0100

    .

--- py/kitalalos.py
+++ py/kitalalos.py
@@ -1,20 +1,20 @@
 a = 0
 b = 15
-c1, c2 = "\N{ESC}[38;255;33;77;99m", "\u001b[0m"
 def f(i): # -nál -nél
     if str(i)[-1] in "124579": return "nél"
     else: return "nál"
 def g(i): #- ra -re
     if str(i)[-1] in "124579": return "re"
     else: return "ra"    
-print(f"Gondoljon egy egész számra amley nem kisebb {c1}{a}{c2}-{f(a)} és nem nagyobb {c1}{b}{c2}-{f(b)}!")
+print(f"Gondoljon egy egész számra amley nem kisebb {a}-{f(a)} és nem nagyobb {b}-{f(b)}!")
 while a != b:
     i = (a + b) // 2
-    v = input(f"A gondolt szám {c1}{i + 1}{c2}-{f(i + 1)} kisebb? ")
+    v = input(f"A gondolt szám {i + 1}-{f(i + 1)} kisebb? (i/n) ")
     if v[0] == "i":
         b = i
     else:
         a = i + 1
     s = ", ".join(map(str, range(a, b + 1)))
     print(f"Éretem, akkor a gondolt szám ezek közül lesz az egyik: {s}")
-print(f"Tehát a {c1}{a}{c2}-{g(a)} gondolt! Remek választás!")
+    print("Hmmm...")
+print(f"Tehát a {a}-{g(a)} gondolt! Remek választás!")
