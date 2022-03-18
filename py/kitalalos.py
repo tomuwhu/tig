6c07185c541545f40268345363bc819b272c6d93
Dr. Németh Tamás / Tamas Nemeth PhD <tomuwhu@gmail.com>
2022-03-18 15:32:54 +0100

    .

--- py/kitalalos.py
+++ py/kitalalos.py
@@ -1,11 +1,19 @@
 a = 0
 b = 15
 def f(i): # 6-nál 7-nél
-    if str(i)[-1] in "0124579": return f"{i}-nél"
-    else: return f"{i}-nál"
+    if i<10 or i % 10 != 0:
+        if str(i)[-1] in "124579": return f"{i}-nél"
+        else: return f"{i}-nál"
+    else:
+        if str(i)[-2] in "14579": return f"{i}-nél"
+        else: return f"{i}-nál"
 def g(i): # 8-ra 10-re
-    if str(i)[-1] in "0124579": return f"{i}-re"
-    else: return f"{i}-ra"    
+    if i<10 or i % 10 != 0:
+        if str(i)[-1] in "124579": return f"{i}-re"
+        else: return f"{i}-ra"
+    else:
+        if str(i)[-2] in "14579": return f"{i}-re"
+        else: return f"{i}-ra"
 print(f"Gondoljon egy egész számra amley nem kisebb {f(a)} és nem nagyobb {f(b)}!")
 while a != b:
     i = (a + b) // 2
