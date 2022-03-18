77b7f63eab1b4a5bbbc10ff49cfba4522af244fe
Dr. Németh Tamás / Tamas Nemeth PhD <tomuwhu@gmail.com>
2022-03-18 15:58:22 +0100

    .

--- py/kitalalos.py
+++ py/kitalalos.py
@@ -1,19 +1,22 @@
-a = 0
-b = 15
-def f(i): # 6-nál 7-nél
+a = 0   # legkisebb gondoloható szám
+ksz = 9 # kérdések száma
+
+b = a + 2 ** ksz - 1 # számoljuk ki a legnagyobb gondolható számot
+def f(i): # ragozás: 6-nál 7-nél
     if i<10 or i % 10 != 0:
         if str(i)[-1] in "124579": return f"{i}-nél"
         else: return f"{i}-nál"
     else:
         if str(i)[-2] in "14579": return f"{i}-nél"
         else: return f"{i}-nál"
-def g(i): # 8-ra 10-re
+def g(i): # ragozás: 8-ra 10-re
     if i<10 or i % 10 != 0:
         if str(i)[-1] in "124579": return f"{i}-re"
         else: return f"{i}-ra"
     else:
         if str(i)[-2] in "14579": return f"{i}-re"
         else: return f"{i}-ra"
+
 print(f"Gondoljon egy egész számra amley nem kisebb {f(a)} és nem nagyobb {f(b)}!")
 while a != b:
     i = (a + b) // 2
@@ -22,7 +25,6 @@ while a != b:
         b = i
     else:
         a = i + 1
-    s = ", ".join(map(str, range(a, b + 1)))
-    print(f"Éretem, akkor a gondolt szám ezek közül lesz az egyik: {s}")
+    print(f"Éretem, akkor a gondolt szám {a}...{b} között lesz valahol.")
     print("Hmmm...")
-print(f"Tehát a {g(a)} gondolt! Remek választás!")
+print(f"Tehát (ha mindig helyesen válaszolt) a {g(a)} gondolt! Remek választás!")
