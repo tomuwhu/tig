441ff730eb4431e43aae7fe6524918f55cf26178
Dr. Németh Tamás / Tamas Nemeth PhD <tomuwhu@gmail.com>
2022-03-28 12:14:26 +0200

    .

--- py/table2.py
+++ py/table2.py
@@ -1,13 +1,8 @@
 from random import shuffle
 l = list(range(1,10))
 shuffle(l)
-a, b, c, d, e, f, g, h, i = l 
-print(f"""
-╔═══╦═══╦═══╗
-║ {a} ║ {b} ║ {c} ║
-╠═══╬═══╬═══╣
-║ {d} ║ {e} ║ {f} ║
-╠═══╬═══╬═══╣
-║ {g} ║ {h} ║ {i} ║
-╚═══╩═══╩═══╝
-""")
+a, b, c, d, e, f, g, h, i = l
+t = "\n╠═══╬═══╬═══╣\n" 
+print("╔═══╦═══╦═══╗")
+print(f"║ {a} ║ {b} ║ {c} ║{t}║ {d} ║ {e} ║ {f} ║{t}║ {g} ║ {h} ║ {i} ║")
+print("╚═══╩═══╩═══╝")
