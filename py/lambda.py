35fec2da95ed4a21beca61723906c5e3e36832d7
Dr. Németh Tamás / Tamas Nemeth PhD <tomuwhu@gmail.com>
2022-03-28 11:36:24 +0200

    .

---
+++ py/lambda.py
@@ -0,0 +1,4 @@
+a = lambda x: x ** 2
+def b(x): 
+    return x ** 3
+print( a(2), b(2), (lambda x: x ** 4)(2) )
--- py/tetelek/filter.py
+++ py/tetelek/filter.py
@@ -1,7 +1,7 @@
 l = [1,2,3,4,5,6,7,8]
 
 # f függvény:
-def f(fn ,iter):
+def f(fn, iter):
     for i in iter:
         if fn(i): yield i
 print(*f(lambda x: x % 2, l))
