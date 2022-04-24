a3e7ae4a3b04fa8df66f5a717aaabef933dcc810
Dr. Németh Tamás / Tamas Nemeth PhD <tomuwhu@gmail.com>
2022-04-24 20:58:41 +0200

    .

---
+++ Brython/szrszt/logic.py
@@ -0,0 +1,29 @@
+from browser import document as D, html as H
+ALPHABET = \
+  "0123456789abcdefghijkl"
+qv = [
+    "kettes","hármas","négyes","ötös","hatos","hetes","nyolcas","kilences","tizes",
+    "tizeneggyes","tizenkettes","tizenhármas","tizennégyes","tizenötös","tizenhatos",
+    "tizenhetes","tizennyocas","tizenkilences","huszas","huszoneggyes","huszonkettes"
+]
+def encode (n):
+  return ALPHABET [n]
+def db (dec = 0, base = 10):
+  if dec < base: return encode (dec)
+  else: return db(dec // base, base) + encode (dec % base)
+S = H.DIV()
+C = H.DIV()
+def f(e):
+    q = int(D["i"].value)
+    S.clear()
+    C.clear()
+    C <= H.H1(["Szorzótábla ", H.I(f"{qv[q-2]}"), " számrendszerhez"])
+    S <= H.TABLE(
+        H.TR(
+            H.TD(db(i*j, q), Class="x" if i == 1 or j == 1 else "x2") for j in range(1,q+1)
+        ) for i in range(1,q+1)
+    )
+D <= C
+D <= H.INPUT(id="i", type="range", min=2, max=22, value=10).bind("change", f)
+f(1)
+D <= S
