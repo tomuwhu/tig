394017a29aaaebb2bbea3999f893ce8e0a580ab9
Dr. Németh Tamás / Tamas Nemeth PhD <tomuwhu@gmail.com>
2022-04-24 11:03:13 +0200

    .

--- Brython/hanoi/logic.py
+++ Brython/hanoi/logic.py
@@ -7,13 +7,9 @@ def h(n, i, j):
         for i in [h(n-1, i, 3-i-j), h(1, i, j), h(n-1, 3-i-j, j)]:
             yield from i 
 
-c = 0
+l = [["❴", H.SPAN(i), "➺", H.SPAN(j), "❵"] for i, j in h(6, 0, 1)]
+l.reverse()
 
-l = [["❴", H.SPAN(i), "➺", H.SPAN(j), "❵"] for i, j in h(4, 0, 1)]
+def f(): D <= H.DIV(l.pop())
 
-def f():
-    global c
-    D <= H.DIV(l[c])
-    c += 1
-
-iv = T.set_interval(f, 1000)
+T.set_interval(f, 1000)
