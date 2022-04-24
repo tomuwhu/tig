40fa13914a90e79b8b212f4b581a62ccecfcd4e1
Dr. Németh Tamás / Tamas Nemeth PhD <tomuwhu@gmail.com>
2022-04-24 21:07:47 +0200

    .

--- Brython/szrszt/logic.py
+++ Brython/szrszt/logic.py
@@ -6,8 +6,7 @@ qv = [
     "tizenegyes","tizenkettes","tizenhármas","tizennégyes","tizenötös","tizenhatos",
     "tizenhetes","tizennyocas","tizenkilences","huszas","huszonegyes","huszonkettes"
 ]
-def encode (n):
-  return ALPHABET [n]
+def encode (n): return ALPHABET [n]
 def db (dec = 0, base = 10):
   if dec < base: return encode (dec)
   else: return db(dec // base, base) + encode (dec % base)
@@ -18,11 +17,9 @@ def f(e):
     S.clear()
     C.clear()
     C <= H.H1(["Szorzótábla ", H.I(f"{qv[q-2]}"), " számrendszerhez"])
-    S <= H.TABLE(
-        H.TR(
-            H.TD(db(i*j, q), Class="x" if i == 1 or j == 1 else "x2") for j in range(1,q+1)
-        ) for i in range(1,q+1)
-    )
+    S <= H.TABLE(H.TR(
+         H.TD(db(i*j, q), Class="x" if i == 1 or j == 1 else "x2") for j in range(1,q+1)
+    ) for i in range(1,q+1))
 D <= C
 D <= H.INPUT(id="i", type="range", min=2, max=22, value=10).bind("change", f)
 f(1)
