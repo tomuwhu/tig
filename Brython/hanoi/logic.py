251ccfe2bc62dfa06063689ad5e99b2f0f2f9f2e
Dr. Németh Tamás / Tamas Nemeth PhD <tomuwhu@gmail.com>
2022-04-24 09:54:31 +0200

    .

--- Brython/hanoi/logic.py
+++ Brython/hanoi/logic.py
@@ -1,18 +1,19 @@
 from browser import document as D, html as H, timer as T
+
 def h(n, i, j):
-    t = ["","⓵","⓶","⓷"]
-    if n == 1:
-        yield t[i], t[j]
+    t = ["⓵","⓶","⓷"]
+    if n == 1: yield t[i], t[j]
     else:
-        yield from h(n - 1, i, 6 - i - j)
-        yield t[i], t[j]
-        yield from h(n - 1, 6 - i - j, j)
-counter = 0
-l = [(i, j) for i, j in h(4, 1, 2)]
+        for i in [h(n-1, i, 3-i-j), h(1, i, j), h(n-1, 3-i-j, j)]:
+            yield from i 
+
+c = 0
+
+l = [["❴", H.SPAN(i), "➺", H.SPAN(j), "❵"] for i, j in h(4, 0, 1)]
 
 def f():
-    global counter
-    D <= H.DIV( " ❴" + H.SPAN(l[counter][0]) + "➺" + H.SPAN(l[counter][1]) + "❵ " )
-    counter += 1
+    global c
+    D <= H.DIV(l[c])
+    c += 1
 
 iv = T.set_interval(f, 1000)
