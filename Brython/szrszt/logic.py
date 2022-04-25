7a2b2381f32539983e88570b6153ba5eee518790
Dr. Németh Tamás / Tamas Nemeth PhD <tomuwhu@gmail.com>
2022-04-25 18:59:10 +0200

    .

--- Brython/szrszt/logic.py
+++ Brython/szrszt/logic.py
@@ -1,18 +1,14 @@
+from contextlib import nullcontext
 from browser import document as D, html as H
-ALPHABET = \
-  "0123456789abcdefghijkl"
-qv = [
-    "kettes","hármas","négyes","ötös","hatos","hetes","nyolcas","kilences","tizes",
-    "tizenegyes","tizenkettes","tizenhármas","tizennégyes","tizenötös","tizenhatos",
-    "tizenhetes","tizennyocas","tizenkilences","huszas","huszonegyes","huszonkettes"
-]
-def encode (n): return ALPHABET [n]
-def db (dec = 0, base = 10):
-  if dec < base: return encode (dec)
-  else: return db(dec // base, base) + encode (dec % base)
+enc = "0123456789abcdefghijkl"
+qv = ["kettes","hármas","négyes","ötös","hatos","hetes","nyolcas","kilences","tizes",
+      "tizenegyes","tizenkettes","tizenhármas","tizennégyes","tizenötös","tizenhatos",
+      "tizenhetes","tizennyocas","tizenkilences","huszas","huszonegyes","huszonkettes"]
+def db (dec, base):
+  return enc[dec] if dec < base else db(dec // base, base) + enc[dec % base]
 S = H.DIV()
 C = H.DIV()
-def f(e):
+def f(e = None):
     q = int(D["i"].value)
     S.clear()
     C.clear()
@@ -22,5 +18,5 @@ def f(e):
     ) for i in range(1,q+1))
 D <= C
 D <= H.INPUT(id="i", type="range", min=2, max=22, value=10).bind("change", f)
-f(1)
+f()
 D <= S
