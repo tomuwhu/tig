893ce9eaa9375304208ccced73c42d8de71d6424
Dr. Németh Tamás / Tamas Nemeth PhD <tomuwhu@gmail.com>
2022-04-25 19:05:41 +0200

    .

--- Brython/szrszt/logic.py
+++ Brython/szrszt/logic.py
@@ -4,15 +4,15 @@ enc = "0123456789abcdefghijkl"
 qv = ["kettes","hármas","négyes","ötös","hatos","hetes","nyolcas","kilences","tizes",
       "tizenegyes","tizenkettes","tizenhármas","tizennégyes","tizenötös","tizenhatos",
       "tizenhetes","tizennyocas","tizenkilences","huszas","huszonegyes","huszonkettes"]
-def db (dec, base):
-  return enc[dec] if dec < base else db(dec // base, base) + enc[dec % base]
+def db(dec, base):
+    return enc[dec] if dec < base else db(dec // base, base) + enc[dec % base]
 S = H.DIV()
 C = H.DIV()
 def f(e = None):
     q = int(D["i"].value)
     S.clear()
     C.clear()
-    C <= H.H1(["Szorzótábla ", H.I(f"{qv[q-2]}"), " számrendszerhez"])
+    C <= H.H1(["Szorzótábla", H.I(f" {qv[q-2]} "), "számrendszerhez"])
     S <= H.TABLE(H.TR(
          H.TD(db(i*j, q), Class="x" if i == 1 or j == 1 else "") for j in range(1,q+1)
     ) for i in range(1,q+1))
