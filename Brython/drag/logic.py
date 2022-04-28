e8a37be1570745bb7b526aa082343917cd528e07
Dr. Németh Tamás / Tamas Nemeth PhD <tomuwhu@gmail.com>
2022-04-28 07:42:39 +0200

    .

--- Brython/drag/logic.py
+++ Brython/drag/logic.py
@@ -1,31 +1,30 @@
 from browser import document as D, html as H
 class ElementMove:
     def __init__(self, m):
-        self.moving = m
-        self.is_moving = False
-        self.moving.bind("mousedown", self.start)
-        self.moving.bind("mouseup", self.stop)
+        self.obj = m
+        self.moving = False
+        m.bind("mousedown", self.start)
+        m.bind("mouseup", self.stop)
         m.style.cursor = "move"
     def start(self, e):
-        self.is_moving = True
-        self.mouse_pos = [e.x, e.y]
-        self.elt_pos = [self.moving.left, self.moving.top]
-        D["x"].style.zIndex = 0
-        D["y"].style.zIndex = 0
-        D["z"].style.zIndex = 0
-        e.target.style.zIndex = 1 
+        self.moving = True
+        self.mp = [e.x, e.y]
+        self.ep = [self.obj.left, self.obj.top]
+        for o in D.select(".m"):
+            if o.style.zIndex: o.style.zIndex = int(o.style.zIndex) - 1
+        e.target.style.zIndex = 4
+        e.target.style.opacity = 0.8
         D.bind("mousemove", self.move)
         e.preventDefault()
     def move(self, e):
-        if self.is_moving: 
-            self.moving.left = self.elt_pos[0] + e.x - self.mouse_pos[0]
-            self.moving.top = self.elt_pos[1] + e.y - self.mouse_pos[1]
+        if self.moving: 
+            self.obj.left = self.ep[0] + e.x - self.mp[0]
+            self.obj.top  = self.ep[1] + e.y - self.mp[1]
     def stop(self, e):
-        self.is_moving = False
+        e.target.style.opacity = 1
+        self.moving = False
         D.unbind("mousemove")
-D <= H.DIV("Fogj meg és vigyél!", id="x")
-ElementMove(D["x"])
-D <= H.DIV("Engem is!", id="y")
-ElementMove(D["y"])
-D <= H.DIV("Szia cica!", id="z")
-ElementMove(D["z"])
+for id, s in [("x","Fogj meg és vigyél!"),("y","Engem is!"),("z","Mozgatható objektum vagyok.")]:
+    el = H.DIV(s, id=id, Class="m")
+    D <= el
+    ElementMove(el)
