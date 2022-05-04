37d06a3142fe1d642a9328cc32db46768fb7447d
Dr. Németh Tamás <tomuwhu@gmail.com>
2022-05-04 14:56:27 +0200

    .

--- Brython/korlevel/logic.py
+++ Brython/korlevel/logic.py
@@ -1,26 +1,21 @@
 from browser import document as D, html as H
-
+SB = H.DIV( Class = "SB" )
+Sx = H.DIV( Class = "Sx" )
 D <= H.DIV((
-    H.HEADER("GYAKBANK - SZEGED"),
-    H.DIV( Class = "SB" ),
-    H.H2( "Fizetési felszólítás", Class="C" ),
-    H.DIV( Class = "SB" ),
-    H.DIV( f"Tisztelt {adat['nev']}!", Class="L" ),
-    H.DIV( Class = "Sx" ),
+    H.HEADER("GYAKBANK - SZEGED"), SB,
+    H.H2( "Fizetési felszólítás", Class="C" ), SB,
+    H.DIV( f"Tisztelt {adat['nev']}!", Class="L" ), Sx,
     H.DIV( f"""
         Tartozása nyilvántartásunk szerint {adat["tartozas"]} Ft, 
         melyet kérünk 1199-1119-34727456 számú bankszámlára befizetni!
-        """, Class="L"),
-    H.DIV( Class = "SB" ),
+        """, Class="L"), SB,
     H.TABLE((
         H.TD(),
         H.TD( "Dr. Árva Aladár Ákos<br>főosztályvezető", Class = "AI" )
-    )),
-    H.DIV( Class = "SB" ),
+    )), SB,
     H.FOOTER( """
         Információ, adategyeztetés: (70) 727-34-56 - http://www.gyakbankszeged.hu
-    """ ),
-    H.HR()
+    """ ), H.HR()
 ) for adat in [
     { "nev": "Kiss Béla", "tartozas": 76325 },
     { "nev": "Nagy Endre Zoltán", "tartozas": 26320 },
