f14ea5f2128224b871868533c9ea206c524820b7
Dr. Németh Tamás <tomuwhu@gmail.com>
2022-05-05 09:01:55 +0200

    .

--- Brython/cards/logic.py
+++ Brython/cards/logic.py
@@ -3,12 +3,11 @@ from random import randint as r
 
 first_charcode, number_of_cards = 127744 + r(0, 200), 100
 
-ol = [  ( chr( first_charcode + i),
-          { "backgroundColor": f"rgb({r(0, 255)}, {r(0, 255)}, {r(0, 255)})", 
-            "color"          : f"rgb({r(0, 255)}, {r(0, 255)}, {r(0, 255)})", 
-            "left"           : r(10, 1200), 
-            "top"            : r(10, 700) }
-        ) for i in range(number_of_cards) ]
+ol = [(chr( first_charcode + i),
+     {"backgroundColor": f"rgb({r(0, 255)}, {r(0, 255)}, {r(0, 255)})", 
+      "color"          : f"rgb({r(0, 255)}, {r(0, 255)}, {r(0, 255)})", 
+      "left"           : r(10, 1200), 
+      "top"            : r(10, 700)}) for i in range(number_of_cards)]
    
 for i, ole in enumerate(ol):
     s, st = ole
