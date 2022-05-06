5e33af6b9e25e50001d07db14179319dabb0faa4
Dr. Németh Tamás <tomuwhu@gmail.com>
2022-05-06 20:43:54 +0200

    .

--- Brython/pd01mo3/logic.py
+++ Brython/pd01mo3/logic.py
@@ -1,15 +1,12 @@
 from browser import document as D, html as H
 def f(e):
-    a = []
-    for k in D.select( "input" ): a.append(int(k.value))
-    RES.clear()
-    RES <= a[0]*a[4]*a[8] + a[1]*a[5]*a[6] + a[2]*a[3]*a[7]
-RES = H.TH('&nbsp', colspan = 3, id = "res")
+    a = [int(k.value) for k in D.select("input")]
+    RES.text = a[0]*a[4]*a[8] + a[1]*a[5]*a[6] + a[2]*a[3]*a[7]
+RES = H.TH(3, colspan = 3)
 T = H.TABLE()
 T <= H.TR(H.TH( "Számoló tábla", colspan = 3))
 T <= [H.TR(H.TD(
-      H.INPUT( value = 1, type = "number", min = 0, max = 9 ))
+      H.INPUT( value = 1, type = "number", min = 0, max = 9 )).bind("change", f)
       for j in range(3)) for i in range(3)]
-T <= H.TR(H.TH(H.BUTTON('Számol').bind("click", f), colspan = 3))
 T <= H.TR(RES)
 D <= T
