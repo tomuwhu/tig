ed749d3cb85963df2c6b6baa262d99e2f6ea635b
Dr. Németh Tamás <tomuwhu@gmail.com>
2022-05-12 17:33:59 +0200

    .

--- sql/bry.py
+++ sql/bry.py
@@ -19,33 +19,36 @@ def list(tx, res):
         RES <= T
     else:
         RES <= H.DIV("Sikeres", Class="done")
-db = W.openDatabase('d', '1.0', 'x', 5*1024*1024)
-def f(e):
-    db.transaction(lambda t: t.executeSql(D['TX'].value, [], list, err))
-def g(e):
-    RES.clear()
-D <= H.H1("Web SQL Playground")
-D <= H.TEXTAREA(id="TX")
-D <= H.HR()
-D <= H.BUTTON("Futtat").bind("click", f)
-D <= H.BUTTON("Reset").bind("click", g)
-RES = H.DIV()
-D <= H.HR()
-D <= RES
-def ins(e):
-    D['TX'].value = e.target.text
-l = [
-    'CREATE TABLE dd (id UNIQUE, name)',
-    'SELECT name, sql from sqlite_master WHERE type = "table" and Length(SQL)<100',
-    'INSERT INTO dd VALUES(1, "Malacka")',
-    'INSERT INTO dd VALUES(2, "Nyuszi")',
-    'INSERT INTO dd VALUES(3, "Micimackó")',
-    'UPDATE dd SET name="Tigris" WHERE id=3',
-    'SELECT * FROM dd ORDER BY name',
-    'SELECT * FROM dd WHERE id<2 ORDER BY name',
-    'DELETE FROM dd',
-    'DROP TABLE dd',
-]
-D <= [
-    H.PRE(li).bind("click", ins) for li in l
-]
+if "openDatabase" in W: 
+    db = W.openDatabase('d', '1.0', 'x', 5*1024*1024)
+    def f(e):
+        db.transaction(lambda t: t.executeSql(D['TX'].value, [], list, err))
+    def g(e):
+        RES.clear()
+    D <= H.H1("Web SQL Playground")
+    D <= H.TEXTAREA(id="TX")
+    D <= H.HR()
+    D <= H.BUTTON("Futtat").bind("click", f)
+    D <= H.BUTTON("Reset").bind("click", g)
+    RES = H.DIV()
+    D <= H.HR()
+    D <= RES
+    def ins(e):
+        D['TX'].value = e.target.text
+    l = [
+        'CREATE TABLE dd (id UNIQUE, name)',
+        'SELECT name, sql from sqlite_master WHERE type = "table" and Length(SQL)<100',
+        'INSERT INTO dd VALUES(1, "Malacka")',
+        'INSERT INTO dd VALUES(2, "Nyuszi")',
+        'INSERT INTO dd VALUES(3, "Micimackó")',
+        'UPDATE dd SET name="Tigris" WHERE id=3',
+        'SELECT * FROM dd ORDER BY name',
+        'SELECT * FROM dd WHERE id<2 ORDER BY name',
+        'DELETE FROM dd',
+        'DROP TABLE dd',
+    ]
+    D <= [
+        H.PRE(li).bind("click", ins) for li in l
+    ]
+else:
+    D <= H.H1("A Web SQL nem támogatott, használjon Chrome böngészőt!")
