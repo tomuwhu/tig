f3488db01a166aa60098d159e25338a549a81ec1
Dr. Németh Tamás <tomuwhu@gmail.com>
2022-05-12 23:02:50 +0200

    .

--- sql/bry.py
+++ sql/bry.py
@@ -29,30 +29,67 @@ if "openDatabase" in W:
     def ins(e):
         D['TX'].value = e.target.text
         RES.clear()
+    def conv(e):
+        LX = D['TX'].value.splitlines()
+        if len(D['tn'].value) and len(LX)>1:
+            FL = LX[0].split(D['sep'].value)
+            DL = map(lambda x: x.split(D['sep'].value), LX[1:])
+            s = f"CREATE TABLE {D['tn'].value} (" + FL[0] + " PRIMARY KEY, "
+            s = f"CREATE TABLE {D['tn'].value} (" + FL[0] + " PRIMARY KEY, " + ", ".join(FL[1:]) + ");\n"
+            for i in DL:
+                s += f"INSERT INTO {D['tn'].value} VALUES(" + ", ".join(map(lambda x: f'"{x}"', i)) + ");\n"
+            D['TX'].value = s
+            nm(1)
+            RES.clear()
+            RES <= H.DIV("Sikeres beolvasás", Class="done")
+        else:
+            RES.clear()
+            RES <= H.DIV("Rövid táblanév vagy hibás input", Class="err")
+    def nm(e):
+        l = [
+            'CREATE TABLE pp (id PRIMARY KEY, name)',
+            'SELECT name, sql FROM sqlite_master WHERE TYPE IS "table" AND LENGTH(sql) < 100',
+            'INSERT INTO pp VALUES(1, "Malacka")',
+            'INSERT INTO pp VALUES(2, "Nyuszi")',
+            'INSERT INTO pp VALUES(3, "Micimackó")',
+            'UPDATE pp SET name = "Tigris" WHERE id = 3',
+            'SELECT * FROM pp ORDER BY name',
+            'ALTER TABLE pp ADD age',
+            'SELECT SUM(id) as Összeg FROM pp',
+            'SELECT * FROM pp WHERE id IN (1, 2, 4) ORDER BY name',
+            'DELETE FROM pp',
+            'DROP TABLE pp',
+        ]
+        MT.clear()
+        MT <= [
+            H.PRE(li).bind("click", ins) for li in l
+        ]
+        BT.clear()
+        BT <= CSVM
+        D["run"].style.visibility = "visible"
+    def cm(e):
+        global LX
+        MT.clear()
+        MT <= H.INPUT(placeholder = "Tábla név", id = "tn")
+        MT <= H.INPUT(placeholder = "Sep", id = "sep", value=";")
+        MT <= H.BUTTON("Beolvas").bind("click", conv)
+        BT.clear()
+        BT <= SM
+        D["run"].style.visibility = "hidden"
     D <= H.H1("SQL Playground")
-    l = [
-        'CREATE TABLE pp (id PRIMARY KEY, name)',
-        'SELECT name, sql FROM sqlite_master WHERE TYPE IS "table" AND LENGTH(sql) < 100',
-        'INSERT INTO pp VALUES(1, "Malacka")',
-        'INSERT INTO pp VALUES(2, "Nyuszi")',
-        'INSERT INTO pp VALUES(3, "Micimackó")',
-        'UPDATE pp SET name = "Tigris" WHERE id = 3',
-        'SELECT * FROM pp ORDER BY name',
-        'ALTER TABLE pp ADD age',
-        'SELECT SUM(id) as Összeg FROM pp',
-        'SELECT * FROM pp WHERE id IN (1, 2, 4) ORDER BY name',
-        'DELETE FROM pp',
-        'DROP TABLE pp',
-    ]
-    D <= [
-        H.PRE(li).bind("click", ins) for li in l
-    ]
+    MT = H.DIV(Class="mv")
+    D <= MT
+    SM = H.BUTTON("SQL mód").bind("click", nm)
+    CSVM = H.BUTTON("CSV mód").bind("click", cm)
+    BT = H.SPAN()
     D <= H.TEXTAREA(id="TX")
     D <= H.HR()
-    D <= H.BUTTON("Futtat").bind("click", f)
+    D <= H.BUTTON("Futtat", id="run").bind("click", f)
     D <= H.BUTTON("Reset").bind("click", g)
+    D <= BT
     RES = H.DIV(Class="res")
     D <= H.HR()
     D <= RES
+    nm(1)
 else:
     D <= H.H1("A Web SQL nem támogatott, használjon Chrome böngészőt!")
