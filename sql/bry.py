6f2c518bcbecd0e275de4ee99d9e38e3da14e53a
Dr. Németh Tamás <tomuwhu@gmail.com>
2022-05-15 10:05:02 +0200

    .

--- sql/bry.py
+++ sql/bry.py
@@ -21,7 +21,7 @@ def list(tx, res):
         ]) for j in range(res.rows.length)]
         RES <= H.PRE(T, Class="l l2")
     else:
-        RES <= H.DIV("Sikeres", Class="done")
+        RES <= H.DIV("Sikeres / Nincs output", Class="done")
     li = D['TX'].value.split(";")
     tli = li[1:]
     if len(tli) > 0:
@@ -42,13 +42,17 @@ def f(e):
         RES <= H.DIV("Üres utasítás", Class="err")
 def ead(tx, res):
     RES.clear()
-    RES <= H.PRE([res.rows.item(i).sql + ";\n" for i in range(res.rows.length)], Class="l")
+    HP = H.PRE([res.rows.item(i).sql + ";\n" for i in range(res.rows.length)], Class="l")
+    RES <= HP
     NL = [ res.rows.item(i).name for i in range(res.rows.length)]
     s = ""
     for name in NL:
         s += f"SELECT * FROM {name};"
     D['TX'].value = s
-    f2()
+    if len(s):
+        f2()
+    else:
+        HP <= H.DIV("Üres adatbázis.", Class="c1")
 def is_number(s):
     try:
         float(s)
@@ -56,7 +60,9 @@ def is_number(s):
     except ValueError:
         return False
 def sznsz(v):
-    if type(v) is int or type(v) is float:
+    if v == "NULL":
+        return 'NULL'
+    elif type(v) is int or type(v) is float:
         return f'{v}'
     elif is_number(v):
         return f'{v}'
@@ -70,14 +76,14 @@ def list2(tx, res):
         keys = W.js.ent(res.rows.item(0))
         for j in range(res.rows.length):
             pre <= f"INSERT INTO {tn} VALUES("
-            pre <= ",".join([sznsz(res.rows.item(j)[i]) for i in keys])
+            pre <= ",".join([sznsz(res.rows.item(j)[i] or "NULL") for i in keys])
             pre <= ");\n"
         RES <= pre
         RES <= H.BR()
     tli = li[1:]
     if len(tli) > 0:
         D['TX'].value = ";".join(tli).strip()
-        Tim.set_timeout(tr2, 100)
+        Tim.set_timeout(tr2, 1)
 def tr2():
     li = D['TX'].value.split(";")
     ali = li[0].strip()
@@ -85,7 +91,7 @@ def tr2():
         db.transaction(lambda t: t.executeSql(ali, [], list2, err))
 def f2():
     if len(D['TX'].value) > 1:
-        Tim.set_timeout(tr2, 100)
+        Tim.set_timeout(tr2, 1)
 def ea(e):
     db.transaction(lambda t: t.executeSql(
         'SELECT name, sql FROM sqlite_master WHERE TYPE IS "table" AND name != "__WebKitDatabaseInfoTable__"',[], ead, err))
@@ -115,8 +121,6 @@ def nm(e):
     l = [
         'CREATE TABLE pp (id PRIMARY KEY, name)',
         'INSERT INTO pp VALUES(1, "Malacka")',
-        'INSERT INTO pp VALUES(2, "Nyuszi")',
-        'INSERT INTO pp VALUES(3, "Micimackó")',
         'UPDATE pp SET name = "Tigris" WHERE id = 3',
         'SELECT * FROM pp ORDER BY name',
         'ALTER TABLE pp ADD age',
@@ -152,7 +156,7 @@ if "openDatabase" in W:
     D <= H.TEXTAREA(id="TX")
     D <= H.HR()
     D <= H.BUTTON("SQL végrehajtása", id="run").bind("click", f)
-    D <= H.BUTTON("<i>db</i> struktúra").bind("click", ldb)
+    D <= H.BUTTON("<i>db</i> struktúra lekérdezése").bind("click", ldb)
     D <= H.BUTTON("Töröl").bind("click", g)
     D <= BT
     D <= H.BUTTON("<i>db</i> => SQL").bind("click", ea)
