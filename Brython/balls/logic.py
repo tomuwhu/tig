19c917b0d8f52ece13dbe3ca84a54aebba821c5e
Dr. Németh Tamás <tomuwhu@gmail.com>
2022-05-20 16:26:02 +0200

    .

--- Brython/balls/logic.py
+++ Brython/balls/logic.py
@@ -3,14 +3,15 @@ from random import randrange as R
 gravity = 10
 number_of_balls = 30
 speed = 20
-flexibility = 99
-SVG = H.SVG(viewBox="0,0,1000,700")
+flexibility = 90
+rolling_resistance = 2
+SVG = H.SVG(viewBox="0, 0, 1000, 700")
 class Golyo:
     def __init__(self, x = 100, y = 100, r = 20, s = "blue", f = "yellow", vx = 1, vy = 1):
         self.vx, self.vy, self.xp, self.yp, self.r = vx, vy, x, y, r
         self.Obj = S.circle(cx = x, cy = y, r = r, stroke = s, fill = f, stroke_width = 3)
-        self.ti = T.set_interval(self.repos, 100 / speed)
-    def repos(self):
+        self.ti = T.set_interval(self.physics, 100 / speed)
+    def physics(self):
         self.xp += self.vx
         self.yp += self.vy
         self.vy += gravity/1000
@@ -20,7 +21,7 @@ class Golyo:
         if self.yp + self.r  > 690:
             if abs(self.vy) < 0.2: self.vy = 0
             self.vy = -self.vy * flexibility / 100
-            self.vx = self.vx * flexibility / 100
+            self.vx = self.vx * (1000-rolling_resistance) / 1000
             self.yp += self.vy / 2
         self.Obj.attrs["cx"] = self.xp
         self.Obj.attrs["cy"] = self.yp
