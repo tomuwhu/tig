585aaf0be035d42c0fb6d0be8e32cb8787311219
Dr. Németh Tamás <tomuwhu@gmail.com>
2022-05-23 12:07:31 +0200

    .

--- nitv/2122/kcs2/fo2/f1/mo.py
+++ nitv/2122/kcs2/fo2/f1/mo.py
@@ -3,4 +3,12 @@ m = [0]*s[0][0]
 for i, j in s[1:]:
     for v in range(i, j):
         m[v] += 1
-print(m[1:].count(0))
+szsz = 0
+usz = False
+for i in m:
+    if not i:
+        if not usz: usz = True
+        else: szsz += 1
+    else: usz = False
+if usz: szsz += 1
+print(szsz)
