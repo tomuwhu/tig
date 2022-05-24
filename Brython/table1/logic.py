e5382ae46e51d453b29afbda9c769dc53b5e9eda
Dr. Németh Tamás <tomuwhu@gmail.com>
2022-05-24 11:17:39 +0200

    .

--- Brython/table1/logic.py
+++ Brython/table1/logic.py
@@ -1,8 +1,5 @@
 from browser import document as D, html as H
-D <= H.H1("Táblázat")
-l = [
-    [1, 2, 5, 9, 8],
-    ["kutya", "zebra", "tyúk", "csacsi", "cica"],
-    range(1, 6)
-]
-D <= H.TABLE(H.TR(H.TD(e) for e in r) for r in l)
+from random import randrange as RR
+D <= H.H1("Sakktábla")
+l = [[(int(i)+int(j)) % 2 for i in "10" * 4] for j in "01" * 4]
+D <= H.TABLE(H.TR(H.TD(" ", Class=f"x{e}") for e in r) for r in l)
