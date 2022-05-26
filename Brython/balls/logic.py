b20dc55ace2694f4ccb730448963311996aa89d2
Dr. Németh Tamás <tomuwhu@gmail.com>
2022-05-26 06:39:54 +0200

    .

--- Brython/balls/logic.py
+++ Brython/balls/logic.py
@@ -9,7 +9,7 @@ SVG = H.SVG(viewBox="0, 0, 1000, 700")
 class Ball:
     def __init__(self, x = 100, y = 300, r = 20, s = "blue", f = "yellow", vx = 1, vy = 1):
         self.vx, self.vy, self.xp, self.yp, self.r = vx, vy, x, y, r
-        self.Obj = S.circle(cx = x, cy = y, r = r, stroke = s, fill = f, stroke_width = 3)
+        self.Body = S.circle(cx = x, cy = y, r = r, stroke = s, fill = f, stroke_width = 3)
         self.ti = T.set_interval(self.physics, 100 / speed)
     def physics(self):
         self.xp += self.vx
@@ -23,10 +23,10 @@ class Ball:
             self.vy = -self.vy * rebound / 100
             self.vx = self.vx * (1000-rolling_resistance) / 1000
             self.yp += self.vy / 2
-        self.Obj.attrs["cx"] = self.xp
-        self.Obj.attrs["cy"] = self.yp
+        self.Body.attrs["cx"] = self.xp
+        self.Body.attrs["cy"] = self.yp
 SVG <= [Ball(r = R(20,40), vx = R(5,70)/40, vy = R(-120,80)/40,
               s = f"rgb({R(256)},{R(256)},{R(256)})", f = f"rgb({R(256)},{R(256)},{R(256)})"
-        ).Obj for _ in range(number_of_balls)]
+        ).Body for _ in range(number_of_balls)]
 SVG <= S.rect(y = 690, width = 1000, height = 10, fill="brown")
 D <= SVG
