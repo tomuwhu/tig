d7701bc5944456a4b8f1e33887cf1d0e576a813d
Dr. Németh Tamás <tomuwhu@gmail.com>
2022-05-27 15:50:24 +0200

    .

--- Brython/chess/logic.py
+++ Brython/chess/logic.py
@@ -1,6 +1,6 @@
 from browser import document as D, html as H
 m = ["f", 0, 0, 0, 0, None]
-def fc(m):
+def fc(m, t):
     if m[0]=="♟" and m[2]==m[4] and m[1]==m[3]-1:
         return True
     if m[0]=="♟" and m[2]==m[4] and m[1]==2 and m[3]==4:
@@ -9,7 +9,15 @@ def fc(m):
         return True
     if m[0]=="♙" and m[2]==m[4] and m[1]==7 and m[3]==5:
         return True
-    return False
+    if m[0]=="♙" and t == "♟" and m[2]==m[4]+1 and m[1]==m[3]+1:
+        return True
+    if m[0]=="♙" and t == "♟" and m[2]==m[4]-1 and m[1]==m[3]+1:
+        return True
+    if m[0]=="♟" and t == "♙" and m[2]==m[4]+1 and m[1]==m[3]-1:
+        return True
+    if m[0]=="♟" and t == "♙" and m[2]==m[4]-1 and m[1]==m[3]-1:
+        return True
+    print(m[:-1], t)
 class CPMove:
     def __init__(self, m):
         self.origcolor = m.style.color
@@ -33,9 +41,6 @@ class CPMove:
         if self.moving:
             el = D.elementFromPoint(e.clientX, e.clientY)
             el.style.visibility = "hidden"
-            ela = D.elementFromPoint(e.clientX, e.clientY)
-            if (ela.className and ela.className[0]=="x"):
-                m[3],m[4] = ela.parent.rowIndex + 1, ela.cellIndex + 1
             el.style.visibility = "visible"
             self.obj.style.color = "rgb(200,50,20)"
             self.obj.left = self.ep[0] + e.x - self.mp[0]
@@ -44,18 +49,20 @@ class CPMove:
         self.obj.style.color = self.origcolor
         e.target.style.visibility = "hidden"
         el = D.elementFromPoint(e.clientX, e.clientY)
+        fig=el.text
         if el.className.split(" ")[0] == "sf":
             el.clear()
             el = D.elementFromPoint(e.clientX, e.clientY)
+        if (el.className and el.className[0]=="x"):
+            m[3],m[4] = el.parent.rowIndex + 1, el.cellIndex + 1
         t = self.obj.innerHTML
-        if not fc(m):
-            D <= (" ?", m[:-1], "? ")
+        if not fc(m, fig):
+            print("?", m[:-1], "?")
         ub = H.SPAN(t, Class=f"sf {'b' if ord(t)>9817 else 'w' }")
         CPMove(ub)
         el.clear()
         el <= ub
         self.moving = False
-
 def f(e):
     el = H.SPAN(e, Class=f"sf {'b' if ord(e)>9817 else 'w' }")
     CPMove(el)
