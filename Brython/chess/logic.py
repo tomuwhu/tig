02e2dd7a23b87801c8d901596ad08cd4a6518e91
Dr. Németh Tamás <tomuwhu@gmail.com>
2022-05-27 06:50:26 +0200

    .

--- Brython/chess/logic.py
+++ Brython/chess/logic.py
@@ -1,51 +1,42 @@
-from browser import document as D, svg as S, html as H
-objlist = []
-lastelement = [0]
-lastzindex = [64]
+from browser import document as D, html as H
 class CPMove:
     def __init__(self, m):
-        m.style.position = "fixed"
         self.origcolor = m.style.color
         self.obj = m
-        objlist.append(self)
         self.moving = False
         m.bind("mousedown", self.start)
         m.bind("mouseup", self.stop)
     def start(self, e):
         self.moving = True
+        self.obj.style.position = "fixed"
+        self.obj.style.fontSize = "55px"
+        self.obj.left -= 18
+        self.obj.top -= 30
         self.mp = [e.x, e.y]
         self.ep = [self.obj.left, self.obj.top]
-        e.target.style.zIndex = lastzindex[0]
-        lastzindex[0] += 1
         D.bind("mousemove", self.move)
     def move(self, e):
-        lastelement[0] = self.obj
         if self.moving: 
             self.obj.style.color = "red"
             self.obj.left = self.ep[0] + e.x - self.mp[0]
             self.obj.top  = self.ep[1] + e.y - self.mp[1]
     def stop(self, e):
-        self.moving = False
         self.obj.style.color = self.origcolor
         e.target.style.visibility = "hidden"
         el = D.elementFromPoint(e.clientX, e.clientY)
-        if el.className == "sf":
+        if el.className.split(" ")[0] == "sf":
             el.clear()
             el = D.elementFromPoint(e.clientX, e.clientY)
-        p = el.getBoundingClientRect()
-        lastelement[0].style.left = p.left + 10
-        lastelement[0].style.top = p.top - 12
-        e.target.style.visibility = "visible"
-        D.unbind("mousemove")
+        t = self.obj.innerHTML
+        ub = H.SPAN(t, Class=f"sf {'b' if ord(t)>9817 else 'w' }")
+        CPMove(ub)
+        el.clear()
+        el <= ub
+        self.clear()
 def f(e):
-    el = H.SPAN(e, Class="sf")
+    el = H.SPAN(e, Class=f"sf {'b' if ord(e)>9817 else 'w' }")
     CPMove(el)
     return el
 D <= H.H1("Sakk") + H.TABLE([
-    H.TR([
-        H.TD(x, Class=f"x{(i+j)%2} {'b' if ord(x.text)>9817 else 'w' }")
-        for i, x in enumerate(map(f, r))]) for j, r in enumerate(
-        ["♜♞♝♛♚♝♞♜", "♟♟♟♟♟♟♟♟"] + [" "*8]*4 + ["♙♙♙♙♙♙♙♙", "♖♘♗♕♔♗♘♖"])])
-for e in objlist:
-    p = e.obj.getBoundingClientRect()
-    e.obj.style.left, e.obj.style.top = p.left - 22, p.top - 42
+    H.TR([H.TD(x, Class=f"x{(i+j)%2}") for i, x in enumerate(map(f, r))])
+    for j, r in enumerate(["♜♞♝♛♚♝♞♜", "♟♟♟♟♟♟♟♟"] + [" "*8]*4 + ["♙♙♙♙♙♙♙♙", "♖♘♗♕♔♗♘♖"])])
