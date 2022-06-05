aa4f37518c7a80065e69fb2ddc2e0692f8bb6518
Dr. Németh Tamás <tomuwhu@gmail.com>
2022-06-05 13:20:54 +0200

    .

--- Brython/svg02/logic.py
+++ Brython/svg02/logic.py
@@ -1,10 +1,10 @@
 from browser import document as D, html as H, svg as S
 D <= H.SVG([
-    S.circle(
+     S.circle(
         cx = 50 + 48 * i, 
         cy = 50 + 21 * i, 
         r  = 40, 
         stroke = "blue",
         fill=f'rgb({50 + 10 * i} ,{250 - 10 * i}, 220)'
-    ) for i in range(20)], 
-    viewBox = "0, 0, 1020, 500")
+     ) for i in range(20)], 
+     viewBox = "0, 0, 1020, 500")
