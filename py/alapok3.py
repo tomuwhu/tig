fcdb2cd9504b64ea1aa0f769d66629d6ee2e3be2
Dr. Németh Tamás <tomuwhu@gmail.com>
2022-06-11 09:01:03 +0200

    .

--- py/alapok3.py
+++ py/alapok3.py
@@ -1,13 +1,11 @@
-from functools import *
+from functools import cmp_to_key as cp, cache as mem
 
 a = [5,3,5,6,7,8,95,32,43,54,23,12,32,43]
 # rendezés egyedi indexxel (két szomszédos elem összemérése)
-a.sort( key = cmp_to_key( lambda a, b: a % 2 - b % 2 ) )
+a.sort( key = cp( lambda a, b: a % 2 - b % 2 ) )
 print( *a )
 
-@cache # rekurzió-memorizálás
-def f(n):
-    return f(n - 1) + f(n - 2) if n>2 else 1
-print(
-    f(45)
-)
+@mem # rekurzió-memorizálás
+def f(n): return f(n - 1) + f(n - 2) if n>2 else 1
+
+print(f(45), f(60))
