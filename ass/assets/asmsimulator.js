9744034cd00988f5e2cf0a77c3900029f6c1011e
Dr. Németh Tamás <tomuwhu@gmail.com>
2022-06-19 19:34:13 +0200

    .

--- ass/assets/asmsimulator.js
+++ ass/assets/asmsimulator.js
@@ -1364,12 +1364,12 @@ var app = angular.module('ASMSimulator', []);
                      {speed: 16, desc: "16 HZ"}];
     $scope.speed = 4;
     $scope.outputStartIndex = 232;
-    $scope.code = `       MOV A, 0x41 ;Set code of "A" into the A
+    $scope.code = `       MOV A, 'A'  ;Set code of 'A' into the A
        MOV B, 0xE8 ;Point to output
 START: MOV [B], A  ;Write the character from A
        INC A       ;Set the next character
        INC B       ;Set the next memory address
-       CMP A, 0x59 ;If A not "Y"
+       CMP A, 'Y'  ;If A not "Y"
        JNZ START   ;Jump to START
        HLT         ;Halt processor`
 
