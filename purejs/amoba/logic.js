e26bd5f41b5a48c17f68df347418863b1b62830e
Dr. Németh Tamás <tomuwhu@gmail.com>
2022-09-29 20:06:33 +0200

    .

---
+++ purejs/amoba/logic.js
@@ -0,0 +1,29 @@
+next = "O", nyert = null, tabla = {}, n = 16
+function f(e) {
+    if (e.innerHTML == "") {
+        next = next == "O" ? "X" : "O"
+        e.innerHTML = next, e.setAttribute('class', next), x = e.cellIndex
+        y = e.parentElement.rowIndex, tabla[x][y] = next
+        setTimeout( () =>
+            [[1,1],[1,0],[0,1],[-1,1]].forEach( direction => {
+                xp=x, yp=y, maxh=0, [vx, vy] = direction
+                while (tabla[xp][yp] === next) xp += vx, yp += vy, maxh++
+                xp=x, yp=y
+                while (tabla[xp][yp] === next) xp -= vx, yp -= vy, maxh++
+                if (maxh > 5) alert(`Nyert: ${next}`), kezd()
+            } 
+        ), 100)
+    }
+}
+function kezd() {
+    tabla = Array(n).fill().map(() => Array(n).fill())
+    document.getElementById('t').innerHTML = `
+    <table>
+        ${Array(n).fill(`
+        <tr>
+            ${Array(n).fill(`<td onmouseup="f(this)"></td>`).join('')}
+        </tr>
+        `).join('')}
+    </table>`
+}
+kezd()
