97c131aa2624b3c7e21fbdfea6267929448772b5
Dr. Németh Tamás <tomuwhu@gmail.com>
2022-09-29 21:47:55 +0200

    .

--- purejs/amoba/logic.js
+++ purejs/amoba/logic.js
@@ -1,22 +1,22 @@
-next = "O", nyert = null, tabla = {}, n = 16
+next = "O", table = {}, n = 16
 function f(e) {
     if (e.innerHTML == "") {
         next = next == "O" ? "X" : "O"
         e.innerHTML = next, e.setAttribute('class', next), x = e.cellIndex
-        y = e.parentElement.rowIndex, tabla[x][y] = next
+        y = e.parentElement.rowIndex, table[x][y] = next
         setTimeout( () =>
             [[1,1],[1,0],[0,1],[-1,1]].forEach( direction => {
                 xp=x, yp=y, maxh=0, [vx, vy] = direction
-                while (tabla[xp][yp] === next) xp += vx, yp += vy, maxh++
+                while (table[xp][yp] === next) xp += vx, yp += vy, maxh++
                 xp=x, yp=y
-                while (tabla[xp][yp] === next) xp -= vx, yp -= vy, maxh++
+                while (table[xp][yp] === next) xp -= vx, yp -= vy, maxh++
                 if (maxh > 5) alert(`Nyert: ${next}`), kezd()
             } 
         ), 100)
     }
 }
-function kezd() {
-    tabla = Array(n).fill().map(() => Array(n).fill())
+function init() {
+    table = Array(n).fill().map(() => Array(n).fill())
     document.getElementById('t').innerHTML = `
     <table>
         ${Array(n).fill(`
@@ -26,4 +26,4 @@ function kezd() {
         `).join('')}
     </table>`
 }
-kezd()
+init()
