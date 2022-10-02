09b097e0b8ffc59fa212249ea90c089d9ebc727e
Dr. Németh Tamás / Tamas Nemeth PhD <tomuwhu@gmail.com>
2022-10-02 12:17:07 +0200

    .

--- py/efmo/2022_1_k/robot.py
+++ py/efmo/2022_1_k/robot.py
@@ -1,11 +1,5 @@
 p = input("Kérem a robot parancsait:")
-def f(c):
-    return len(list(filter(lambda x: x == c, p)))
-for b in "EDKN":
-    print(b, "betűk száma:", f(b))
-s = ""
-if (f("E")>f("D")): s += "E" * (f("E") - f("D"))
-else:               s += "D" * (f("D") - f("E"))
-if (f("K")>f("N")): s += "K" * (f("K") - f("N"))
-else:               s += "N" * (f("N") - f("K"))
-print("Egy legrövidebb út parancsszava:", s)
+def f(c): return len(list(filter(lambda x: x == c, p)))
+def mo(a, b): return a * (f(a) - f(b)) if f(a) > f(b) else  b * (f(b) - f(a))
+for b in "EDKN": print(b, "betűk száma:", f(b))
+print("Egy legrövidebb út parancsszava:", mo("E", "D") + mo("K", "N"))
