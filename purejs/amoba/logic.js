45e3b25c44775ca20bb213ebb5ffc4672b89edc3
Dr. Németh Tamás <tomuwhu@gmail.com>
2022-10-02 20:34:49 +0200

    .

--- purejs/amoba/logic.js
+++ purejs/amoba/logic.js
@@ -3,15 +3,13 @@ f = e => { if (e.innerHTML == "") {
     next = next == "O" ? "X" : "O"
     e.innerHTML = next, e.setAttribute('class', next), x = e.cellIndex
     y = e.parentElement.rowIndex, table[x][y] = next
-    setTimeout( () =>
-        [[1,1],[1,0],[0,1],[-1,1]].forEach( direction => {
-            xp=x, yp=y, maxh=0, [vx, vy] = direction
-            while (table[xp][yp] === next) xp += vx, yp += vy, maxh++
-            xp=x, yp=y
-            while (table[xp][yp] === next) xp -= vx, yp -= vy, maxh++
-            if (maxh > 5) alert(`Nyert: ${next}`), init()
-        } 
-    ), 100)
+    setTimeout(() => [[1,1],[1,0],[0,1],[-1,1]].forEach( d => {
+        xp=x, yp=y, maxh=0, [vx, vy] = d
+        while (table[xp][yp] === next) xp += vx, yp += vy, maxh++
+        xp=x, yp=y
+        while (table[xp][yp] === next) xp -= vx, yp -= vy, maxh++
+        if (maxh > 5) alert(`Nyert: ${next}`), init()
+    }), 100)
 }}
 (init = () => {
     table = Array(n).fill().map(() => Array(n).fill())
@@ -19,7 +17,7 @@ f = e => { if (e.innerHTML == "") {
     <table>
         ${Array(n).fill(`
         <tr>
-            ${Array(n).fill(`<td onmouseup="f(this)" />`).join('')}
+        ${Array(n).fill(`<td onmouseup="f(this)" />`).join('')}
         </tr>
         `).join('')}
     </table>`
