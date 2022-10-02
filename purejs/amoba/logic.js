bc68a7fcd7747ebc3d13b855c5ae778d5e3f551e
Dr. Németh Tamás <tomuwhu@gmail.com>
2022-10-02 20:33:14 +0200

    .

--- purejs/amoba/logic.js
+++ purejs/amoba/logic.js
@@ -1,20 +1,18 @@
 next = "O", n = 16
-f = e => {
-    if (e.innerHTML == "") {
-        next = next == "O" ? "X" : "O"
-        e.innerHTML = next, e.setAttribute('class', next), x = e.cellIndex
-        y = e.parentElement.rowIndex, table[x][y] = next
-        setTimeout( () =>
-            [[1,1],[1,0],[0,1],[-1,1]].forEach( direction => {
-                xp=x, yp=y, maxh=0, [vx, vy] = direction
-                while (table[xp][yp] === next) xp += vx, yp += vy, maxh++
-                xp=x, yp=y
-                while (table[xp][yp] === next) xp -= vx, yp -= vy, maxh++
-                if (maxh > 5) alert(`Nyert: ${next}`), init()
-            } 
-        ), 100)
-    }
-}
+f = e => { if (e.innerHTML == "") {
+    next = next == "O" ? "X" : "O"
+    e.innerHTML = next, e.setAttribute('class', next), x = e.cellIndex
+    y = e.parentElement.rowIndex, table[x][y] = next
+    setTimeout( () =>
+        [[1,1],[1,0],[0,1],[-1,1]].forEach( direction => {
+            xp=x, yp=y, maxh=0, [vx, vy] = direction
+            while (table[xp][yp] === next) xp += vx, yp += vy, maxh++
+            xp=x, yp=y
+            while (table[xp][yp] === next) xp -= vx, yp -= vy, maxh++
+            if (maxh > 5) alert(`Nyert: ${next}`), init()
+        } 
+    ), 100)
+}}
 (init = () => {
     table = Array(n).fill().map(() => Array(n).fill())
     document.getElementById('t').innerHTML = `
