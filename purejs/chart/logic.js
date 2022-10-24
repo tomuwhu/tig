cfdee0639bca63bbee59964e46b9da70ff9cbab6
Dr. Németh Tamás <tomuwhu@gmail.com>
2022-10-24 21:34:20 +0200

    .

--- purejs/chart/logic.js
+++ purejs/chart/logic.js
@@ -4,11 +4,22 @@ const myChart = new Chart(ctx, {
     data: {
         labels: ['Opel', 'Ford', 'Subaru', 'Suzuki', 'Mazda', 'Skoda'],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 7],
+            label: 'Szavazatok száma',
+            data: [12, 19, 7, 5, 12, 14],
             backgroundColor: 'rgba(54, 162, 235, 0.2)',
             borderColor: 'rgba(54, 162, 235, 1)',
             borderWidth: 1
         }]
     }
 })
+function f() {
+    myChart.data.datasets.push({
+            label: 'Eladási számok',
+            data: [17, 16, 3, 15, 8, 17],
+            backgroundColor: 'rgba(154, 62, 135, 0.2)',
+            borderColor: 'rgba(154, 62, 135, 1)',
+            borderWidth: 1
+    })
+    myChart.update()
+    document.getElementById("g").style.display = "none"
+}
