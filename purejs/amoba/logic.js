d717671cd1ae720153c7c5255b0b67318a517d54
Dr. Németh Tamás <tomuwhu@gmail.com>
2022-11-03 16:30:22 +0100

    .

--- purejs/amoba/logic.js
+++ purejs/amoba/logic.js
@@ -4,15 +4,15 @@ f = e => { if (e.innerHTML == "") {
     e.innerHTML = nx, e.setAttribute('class', nx)
     x = e.cellIndex, y = e.parentElement.rowIndex, t[x][y] = nx
     setTimeout(() => [[1,1],[1,0],[0,1],[-1,1]].forEach( d => {
-        xp=x, yp=y, maxh=0, [vx, vy] = d
-        while (t[xp][yp] === nx) xp += vx, yp += vy, maxh++
-        xp=x, yp=y
-        while (t[xp][yp] === nx) xp -= vx, yp -= vy, maxh++
+        xp = x, yp = y, maxh = 0, [vx, vy] = d
+        while (t[xp] && t[xp][yp] === nx) xp += vx, yp += vy, maxh++
+        xp = x, yp = y
+        while (t[xp] && t[xp][yp] === nx) xp -= vx, yp -= vy, maxh++
         if (maxh > 5) alert(`Nyert: ${nx}`), init()
     }), 100)
 }}
 (init = () => {
-    t = Array(n).fill().map(() => Array(n).fill())
+    t = Array(n).fill().map(() => Array(n-1).fill())
     document.getElementById('t').innerHTML = `
     <table>
         ${Array(n).fill(`
