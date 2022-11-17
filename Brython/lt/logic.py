49b3faec37d3f5e41f1b0b021377c4c8fed2c14d
Dr. Németh Tamás <tomuwhu@gmail.com>
2022-11-17 14:39:31 +0100

    .

--- Brython/lt/logic.py
+++ Brython/lt/logic.py
@@ -1,2 +1,11 @@
 from browser import document as D, html as H
-D <= H.H1("Hello, World!")
+def f(e):
+    r = e.target.style
+    if r.backgroundColor == "red":
+        r.backgroundColor = "blue"
+        r.color = "yellow"
+    else:
+        r.backgroundColor = "red"
+        r.color = "white"
+D <= H.H1("Cím")
+D <= H.DIV([H.DIV(f"Katt {i}").bind("click", f) for i in range(10)], Class="c")
