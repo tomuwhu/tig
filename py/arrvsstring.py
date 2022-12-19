7d93310406489f362479c8a1cc370a30d89b1798
Dr. Németh Tamás <tomuwhu@gmail.com>
2022-12-19 11:58:11 +0100

    .

--- py/arrvsstring.py
+++ py/arrvsstring.py
@@ -1,11 +1,17 @@
 a = [1, 2, 3]
-b = a # csak referencia
+b = a # b csak referencia (a-ra mutató pointer)
+a.append(5)
+print(b)
+print(a)
+
+a = [1, 2, 3]
+b = list(a) # b másolat (új váltaozó saját memóriaterülettel)
 a.append(5)
 print(b)
 print(a)
 
 a = "cica"
-b = a # másolat
+b = a # b másolat (új váltaozó saját memóriaterülettel)
 a += "mica"
 print(b)
 print(a)
