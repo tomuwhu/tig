800ecb524bb466c29727bc8e846919170c77ab7c
Dr. Németh Tamás <tomuwhu@gmail.com>
2022-12-23 23:26:36 +0100

    .

--- py/alg_eukl1.py
+++ py/alg_eukl1.py
@@ -1,10 +1,6 @@
 def f(a, b):
-    if a == b:
-        return a
-    elif a > b:
-        return f(a - b, b)
-    else:
-        return f(a, b - a)
+    if a > b: a, b = b, a
+    return f(a, b - a) if a < b else a
 print(f(5700,1900))
 print(f(5700,2900))
 print(f(5700,3900))
