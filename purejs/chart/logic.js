fbd8645d6eb6ca5936e8fa79e0a4f83fc6ba51ff
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-02-10 11:50:07 +0100

    .

--- purejs/chart/logic.js
+++ purejs/chart/logic.js
@@ -1,52 +1,31 @@
 const ctx = document.getElementById('myChart')
-ksz = 1
-dsz = 10
-data = [0, 0, 0, 0, 0, 0]
-const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: [1, 2, 3, 4, 5, 6],
-        datasets: [{
-            label: `Dobások összegének darabszáma ${ksz} kockával ${dsz} dobás után`,
-            data: [0, 0, 0, 0, 0, 0],
-            backgroundColor: 'rgba(54, 162, 235, 0.2)',
-            borderColor: 'rgba(54, 162, 235, 1)',
-            borderWidth: 1
-        }]
-    }
-})
-function f(v) {
-    ksz = Number(v)
-    document.getElementById("ksz").innerHTML = ksz
-    rd()
+ksz = 1, dsz = 10, data = Array(6).fill(0)
+ds  = {
+    backgroundColor: 'rgba(54, 162, 235, 0.2)',
+    borderColor: 'rgba(54, 162, 235, 1)',
+    borderWidth: 1
 }
-function g(v) {
-    dsz = 10 ** Number(v)
-    document.getElementById("dsz").innerHTML = dsz
-    rd()
-}
-function rd() {
+lab = (ksz, dsz) =>`Dobások összegének darabszáma ${ksz} kockával ${dsz} dobás után`
+const myChart = new Chart(ctx, { type: 'bar', data: {
+    labels: data.map((v, i) => i + ksz),
+    datasets: [{ label: lab(ksz, dsz), data, ...ds }]
+}})
+f = v => ( ksz = Number(v), document.getElementById("ksz").innerHTML = ksz, r.d() )
+g = v => ( dsz = 10 ** Number(v), document.getElementById("dsz").innerHTML = dsz, r.d() )
+r = { d() {
     data = Array(ksz*5+1).fill(0)
-    for (i=0; i < dsz; i++) {
-        dobo = 0
-        for (j=0; j < ksz; j++) {
-            dobas = Math.floor(Math.random()*6 + 1)
-            dobo += dobas 
+    for (i = 0; i < dsz; i++) {
+        összeg = 0
+        for (j = 0; j < ksz; j++) {
+            dobas = Math.floor(Math.random() * 6 + 1)
+            összeg += dobas 
         }
-        data[dobo-ksz]++
+        data[összeg - ksz]++
     }
     myChart.data = {
         labels: data.map((v, i) => i + ksz),
-        datasets: [{
-            label: `Dobások összegének darabszáma ${ksz} kockával ${dsz} dobás után`,
-            data: data,
-            backgroundColor: 'rgba(54, 162, 235, 0.2)',
-            borderColor: 'rgba(54, 162, 235, 1)',
-            borderWidth: 1
-        }]
+        datasets: [{ label: lab(ksz, dsz), data, ...ds }]
     }
     myChart.update()
-}
-setTimeout(() => {
-    rd()
-}, 1000);
+}}
+setTimeout(() => r.d(), 1000);
