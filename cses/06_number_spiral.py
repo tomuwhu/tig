9e599975fd95db6aa639ed52320e910e84b7f637
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-02-19 12:35:18 +0100

    .

--- cses/06_number_spiral.py
+++ cses/06_number_spiral.py
@@ -5,7 +5,9 @@ for i in range(t):
     k = (m - 1) ** 2
     k2 = (m - 1) * 2 + 1
     l = min(x, y)
-    if k2 % 2:
+    if x > y: p = 0
+    else: p = 1
+    if (m + p) % 2:
        print(k + k2 - l + 1)
     else:
        print(k + l)
