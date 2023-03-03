cc196c3d8138ec9ac17b127f5c23501b9c2ce4e8
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-03-03 18:45:34 +0100

    .

--- py/range_while.py
+++ py/range_while.py
@@ -1,4 +1,8 @@
-def f(i = 0, n = None, s = 1):
+def f(i=0, n=None, s=1):
     (n, i) = (i, 0) if n == None else (n, i)
-    while i < n: yield i; i += s
-[print(*list(i)) for i in [f(10), f(3, 7), f(3, 17, 3)]]
+    while i < n: print(i); i += s
+f(10)
+print("---")
+f(3, 7)
+print("---")
+f(3, 17, 3)
--- py/range_while2.py
+++ py/range_while2.py
@@ -1,11 +1,12 @@
 def f(i, n=0, s=1):
-    if n==0: (i, n) = (n, i)
+    if n == 0:
+        (i, n) = (n, i)
     while i < n:
         yield i
         i += s
 print(
-    f(10), list( f(10) ), *map(lambda x:f'{x} - {x ** 2}',f(10)), sep=" ;\n", end=" ; \n------\n"
+    f(10), list(f(10)), *map(lambda x: f'{x} - {x ** 2}', f(10)), sep=" ;\n", end=" ; \n------\n"
 )
 print(
-    list( f(3, 10, 2) ), 7 in f(3, 10, 2), sep="\n"
+    list(f(3, 10, 2)), 7 in f(3, 10, 2), sep="\n"
 )
