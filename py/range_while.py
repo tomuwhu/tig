e342da6553aaae123581bc6a24ba3cc2ad46a19c
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-03-03 18:26:15 +0100

    .

--- py/range_while.py
+++ py/range_while.py
@@ -1,8 +1,4 @@
 def f(i = 0, n = None, s = 1):
     (n, i) = (i, 0) if n == None else (n, i)
-    while i < n: print(i); i+=s
-f(10)
-print("---")
-f(3, 7)
-print("---")
-f(3, 17, 3)
+    while i < n: yield i; i += s
+[print(*list(i)) for i in [f(10), f(3, 7), f(3, 17, 3)]]
