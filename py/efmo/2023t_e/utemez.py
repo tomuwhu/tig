7164c8b7aeeee3f59e4769a684f4cb4effd73a85
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-05-28 20:08:37 +0200

    .

--- py/efmo/2023t_e/utemez.py
+++ py/efmo/2023t_e/utemez.py
@@ -1,15 +1,15 @@
-def sorszam(h, n): return 16 + [0, 30, 31][h - 6] + n
+def sorszam(h, n): return [0, 30, 61][h - 6] + n - 15
 def f(x):
     t = x.split()
     h1, h2 = sorszam(int(t[0]),int(t[1])), sorszam(int(t[2]),int(t[3]))
     return {"t": t[5], "k": h1, "v": h2, "d": list(t[4]), "orig": t}
 l = list(map(f, open("taborok.txt").read().split("\n")[:-1]))
-print(f"""\n2. feladat\nAz adatsorok száma: {len(l)}
+print(f"""2. feladat\nAz adatsorok száma: {len(l)}
 Az először rögzített tábor témája: {l[0]["t"]}
 Az utoljára rögzített tábor témája: {l[-1]["t"]}""")
 ztl = list(filter(lambda x: x["t"] == "zenei", l))
 s = "\n".join([f"kezdődik {i['orig'][0]}. hó {i['orig'][1]}" for i in ztl])
-print(f"\n3. feladat\n{s if s else f'Nem volt zenei tábor'}")
+print(f"3. feladat\n{s if s else f'Nem volt zenei tábor'}")
 lr = sorted(l, key=lambda x: len(x["d"]), reverse=1)
 h, i = len(lr[0]["d"]), 0
 print("4. feladat\nLegnépszerűbbek:")
@@ -17,6 +17,7 @@ while h == len(lr[i]["d"]):
     s = f'{lr[i]["orig"][0]} {lr[i]["orig"][1]} {lr[i]["t"]}'
     print(s)
     i += 1
+print("6. feladat")
 ho, nap = input("hó: "), input("nap: ")
 hl = sorszam(int(ho),int(nap))
 if ho == 2: hl += 1
@@ -29,6 +30,7 @@ with open('egytanulo.txt', 'w') as f:
         f"{i['orig'][0]}.{i['orig'][1]}-{i['orig'][2]}.{i['orig'][3]} {i['t']}"
         for i in mt
     ]))
+print("7. feladat")
 e, vu = 0, 0
 for next in [[x["k"], x["v"]] for x in mt]:
     if next[0] < e: vu += 1
