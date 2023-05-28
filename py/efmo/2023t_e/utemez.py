3c8e3e327f1f66fd84a423523acd263001e4053f
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-05-28 19:19:45 +0200

    .

--- py/efmo/2023t_e/utemez.py
+++ py/efmo/2023t_e/utemez.py
@@ -3,21 +3,18 @@ def f(x):
     h1 = (int(t[0])-6)*30 + int(t[1])
     h2 = (int(t[2])-6)*30 + int(t[3])
     if t[0]<t[2] and t[2]==8: h2 += 1 # az augusztus 31 napos
-    return {"t": t[5], "k": h1, "v": h2, "it": h2-h1+1, "d": list(t[4]),
+    return {"t": t[5], "k": h1, "v": h2, "d": list(t[4]),
             "ks": f"kezdődik {t[0]}. hó {t[1]}. napján", "orig": t}
 l = list(map(f, open("taborok.txt").read().split("\n")[:-1]))
 n = len(l)
 print(f"""\n2. feladat\nAz adatsorok száma: {n}
 Az először rögzített tábor témája: {l[0]["t"]}
 Az utoljára rögzített tábor témája: {l[-1]["t"]}""")
-tn = "zenei"
-ztl = list(filter(lambda x: x["t"] == tn, l))
+ztl = list(filter(lambda x: x["t"] == "zenei", l))
 s = "\n".join([i["ks"] for i in ztl])
-""
 print(f"\n3. feladat\n{s if s else f'Nem volt {tn} tábor'}")
 lr = sorted(l, key=lambda x: len(x["d"]), reverse=1)
-h = len(lr[0]["d"])
-i = 0
+h, i = len(lr[0]["d"]), 0
 print("4. feladat\nLegnépszerűbbek:")
 while h == len(lr[i]["d"]):
     s = f'{lr[i]["orig"][0]} {lr[i]["orig"][1]} {lr[i]["t"]}'
