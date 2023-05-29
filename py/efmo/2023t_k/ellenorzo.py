f5064170c25362167c971a53f247dfbe10459373
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-05-29 19:41:43 +0200

    .

--- py/efmo/2023t_k/ellenorzo.py
+++ py/efmo/2023t_k/ellenorzo.py
@@ -2,7 +2,5 @@ s  = input("Kérem a TAJ-számot: ")
 s1, s2 = s[0:-1:2], s[1:-1:2]
 sz = lambda s, n: sum(map(lambda x: int(x) * n, s))
 szo = sz(s1, 3) + sz(s2, 7)
-print("Az ellenőrző számjegy:", szo % 10)
-print("A szorzatok összege:", szo)
-print("Helyes a szám" if int(s[8]) == szo % 10 
-else  "Hibás a szám!")
+print(f"Az ellenőrző számjegy: {szo % 10 }\nA szorzatok összege: {szo}\n" +
+("Helyes a szám" if int(s[8]) == szo % 10 else "Hibás a szám!"))
