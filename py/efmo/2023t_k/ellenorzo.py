63f6cdac4eba334bcf83123df257a24caf90706e
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-05-29 19:54:05 +0200

    .

--- py/efmo/2023t_k/ellenorzo.py
+++ py/efmo/2023t_k/ellenorzo.py
@@ -1,7 +1,7 @@
 # 1 a TAJ-szám beolvasása
 s  = input("Kérem a TAJ-számot: ")
 # s1 a páratlan indexű s2 a páros indexű számjegyek
-s1, s2 = s[0:-1:2], s[1:-1:2]
+s1, s2 = s[0:-1:2], s[1:-1:2] # a [0] az 1. elem tehát az páratlan, az [1] a második ...
 # sz függvény összegzi a kapott számjegyek n-szeresét
 sz = lambda s, n: sum(map(lambda x: int(x) * n, s))  
 # az ellenőrző összeg a páratlan indexű szájegyek 3-szorosa + a páros indexű számjegyek 7-szerese
