216f28802ba0b32bce42882b166f6436ad02b136
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-09-12 11:38:05 +0200

    .

--- cses/13_gray_code.py
+++ cses/13_gray_code.py
@@ -1,14 +1,13 @@
 n = int(input())
 
-def gc(t, i):
+def gc(i, t=[""]):
     if i == 0:
         return t
     else:
         t2 = t[:]
         t2.reverse()
         return gc(
-            list(map(lambda s: "0" + s, t)) + list(map(lambda s: "1" + s, t2)), i - 1
+            i - 1, list(map(lambda s: "0" + s, t)) + list(map(lambda s: "1" + s, t2))
         )
 
-print(*gc([""], n), sep="\n")
-
+print(*gc(n), sep="\n")
