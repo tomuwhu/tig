ad5f0e10b5b94d4faca061948ee58dd1e24858f7
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-10-08 21:09:49 +0200

    .

--- py/gyufa.py
+++ py/gyufa.py
@@ -2,12 +2,9 @@
 Gyufaszálas háromszöges feladat megoldása az első óráról (beugró feladat)
 """
 n = int(input())
-print(
-    *{
-        ",".join(map(str, [i, j, n - i - j]))
-        for i in range(1, n // 2)
-        for j in range(i, n // 2)
-        if 2 * (i + j) > n
-    },
-    sep="\n"
-)
+print( *{
+    ",".join(map(str, sorted([i, j, n - i - j])))
+    for i in range(1, n // 2)
+    for j in range(i, n // 2 + 1)
+    if 2 * (i + j) > n
+}, sep="\n" )
