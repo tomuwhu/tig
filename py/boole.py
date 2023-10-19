022e2a97bc3579adee35937e01a33ac7008f0cf5
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-10-19 21:59:33 +0200

    .

--- py/boole.py
+++ py/boole.py
@@ -1,10 +1,9 @@
 rf = ['false', 'true', "and", "not", "xor", "or"]
-s = input()
-s, so = s.lower(), s.upper()
+s = input(); s, su = s.lower(), s.upper()
 for i, v in enumerate(rf): s = s.replace(v, str(i))
 vl = list(filter(lambda x: x in s, "abcdefghijklmnoprstuvwxyz"))
 for i, v in enumerate(rf[2:]): s = s.replace(str(i + 2), v).replace('xor', '!=')
-print(f"Q = {so}")
+print(f"Q = {su}")
 try:
     l = eval(f"""[[{", ".join(vl)}, eval(s)] {" ".join(map(lambda x: f"for {x} in [0, 1]", vl))}]""" )
     for row in sum([[sum([list(map(lambda x: x.upper(), vl)), ["Q"]], [])], l], []):
